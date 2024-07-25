6bb908a1b5e288684225af705ff44194965a22a6
Charly <carlosgarita3110@gmail.com>
2024-07-25 08:32:47 -0600

    feat: add PopupComponent for displaying popups in the management client

---
+++ management_client/src/popups/PopupComponent.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import '../assets/styles/popup/popup.css'
+
+const PopupComponent = ({ message, onClose, type }) => {
+  const popupClass = type === 'confirmation' ? 'confirmation' : 'error'
+
+  return (
+    <div className='overlay'>
+      <div className={`popup ${popupClass}`}>
+        <label htmlFor='toggle' className='close-btn' onClick={onClose}>&times;</label>
+        <p className='message'>{message}</p>
+      </div>
+    </div>
+  )
+}
+
+export default PopupComponent
