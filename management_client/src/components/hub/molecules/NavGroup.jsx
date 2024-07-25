523f10422400fb6f986116078c488c19d6ee7c32
Charly <carlosgarita3110@gmail.com>
2024-07-25 16:38:43 -0600

    feat(hub): add NavGroup component for displaying navigation groups in the hub

---
+++ management_client/src/components/hub/molecules/NavGroup.jsx
@@ -0,0 +1,51 @@
+// src/components/NavGroup.js
+import React from 'react'
+import Icon from '../atoms/Icon'
+import NavItem from '../atoms/NavItem'
+import NavLink from '../atoms/NavLink'
+import menuConfig from './menuConfig' // Importa la configuración del menú
+import '../../../assets/styles/hub/nav-group.css'
+
+const NavGroup = ({ currentPage }) => {
+  return (
+    <div className='left-side-nav-container'>
+      {menuConfig.map((menu) => (
+        <NavItem key={menu.title}>
+          <div className='side-nav-group-header-label'>
+            <Icon>{menu.icon}</Icon>
+            <p>{menu.title}</p>
+          </div>
+          <ul className='menu-group-label'>
+            {menu.items.map((item) => (
+              <NavItem key={item.key} className={item.subItems ? 'has-submenu' : ''}>
+                <NavLink
+                  to={item.path}
+                  className={currentPage.startsWith(item.key) ? 'active' : ''}
+                  onClick={item.onClick || null}
+                >
+                  {item.label}
+                </NavLink>
+                {item.subItems && (
+                  <ul className='submenu'>
+                    {item.subItems.map((subItem) => (
+                      <NavItem key={subItem.key}>
+                        <NavLink
+                          to={subItem.path}
+                          className={currentPage === subItem.key ? 'active' : ''}
+                        >
+                          {subItem.label}
+                        </NavLink>
+                      </NavItem>
+                    ))}
+                  </ul>
+                )}
+              </NavItem>
+            ))}
+          </ul>
+        </NavItem>
+      ))}
+    </div>
+  )
+}
+
+export default NavGroup
