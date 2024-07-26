5128829c4acb22214c8b139be27de99e16a567d6
Charly <carlosgarita3110@gmail.com>
2024-07-25 18:47:03 -0600

    refactor(hub): update menuConfig import path in HubRouter component

--- management_client/src/components/hub/router/HubRouter.jsx
+++ management_client/src/components/hub/router/HubRouter.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import HubViewComponent from '../view/HubViewComponent'
-import menuConfig from '../molecules/menuConfig'
+import menuConfig from '../config/menuConfig'
 
 const HubRouter = () => {
   return (
