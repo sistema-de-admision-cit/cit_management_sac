80b02c69b8fea91097437acfb7cc33e7bbd0bb28
Charly <carlosgarita3110@gmail.com>
2024-07-27 13:07:13 -0600

    feat(views): add AddQuestionView for displaying AddQuestionForm
    - Created AddQuestionView component to display the AddQuestionForm.
    - Applied styles from view.css for layout and design.
    - Wrapped AddQuestionForm within a section-container div for consistent styling.

---
+++ management_client/src/components/questions/add_questions/view/AddQuestionView.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import AddQuestionForm from '../organisms/AddQuestionForm'
+import '../../../../assets/styles/questions/view.css'
+
+const AddQuestionView = () => {
+  return (
+    <div className='section-container'>
+      <AddQuestionForm />
+    </div>
+  )
+}
+
+export default AddQuestionView
