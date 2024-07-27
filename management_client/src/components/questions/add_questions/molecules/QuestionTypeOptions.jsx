0d74a5d356c88483ce31de57d55a7bf738e5726b
Charly <carlosgarita3110@gmail.com>
2024-07-27 13:06:21 -0600

    feat(atoms): add QuestionTypeOptions atom for question type selection
    - Created QuestionTypeOptions atom using InputField for selecting question type.
    - Added radio-group type InputField to manage question type options.
    - Passed value and handleChange props for state management.

---
+++ management_client/src/components/questions/add_questions/molecules/QuestionTypeOptions.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import InputField from '../../../global/atoms/InputField'
+
+const QuestionTypeOptions = ({ value, handleChange, options }) => (
+  <InputField
+    field={{ name: 'questionType', label: 'Tipo de Pregunta', type: 'radio-group', options }}
+    value={value}
+    handleChange={handleChange}
+    className='form-group'
+  />
+)
+
+export default QuestionTypeOptions
