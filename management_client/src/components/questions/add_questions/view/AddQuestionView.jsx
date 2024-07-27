0cb7268cfde021da02e492eaf43fc33cab51aa46
Charly <carlosgarita3110@gmail.com>
2024-07-27 15:04:20 -0600

    feat: add title tag to AddQuestionView
    The `AddQuestionView` component was modified to include a `<title>` tag with the text "Agregar Pregunta".

--- management_client/src/components/questions/add_questions/view/AddQuestionView.jsx
+++ management_client/src/components/questions/add_questions/view/AddQuestionView.jsx
@@ -2,9 +2,11 @@ import React from 'react'
 import AddQuestionForm from '../organisms/AddQuestionForm'
 import '../../../../assets/styles/questions/view.css'
 
+// <title> es el título que aparece en la pestaña del navegador
 const AddQuestionView = () => {
   return (
     <div className='section-container'>
+      <title>Agregar Pregunta</title>
       <AddQuestionForm />
     </div>
   )
