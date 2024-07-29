61c0cc87d58c4f639702a7092240f7701b52dc78
Charly <carlosgarita3110@gmail.com>
2024-07-29 16:50:17 -0600

    refactor: enhance FindQuestion component with advanced search functionality
    - Replaced handleModifyForSearch and handleDeleteForSearch with handleSearch and handleSearchByCode
    - Added state for advanced search options and search code
    - Introduced ExamTypeOptions component for selecting exam type
    - Added button to toggle advanced search options
    - Updated logic to handle query-based and advanced search scenarios
    - Modified UI to include advanced search fields and options

--- management_client/src/components/questions/base/molecules/FindQuestion.jsx
+++ management_client/src/components/questions/base/molecules/FindQuestion.jsx
@@ -1,33 +1,37 @@
 import React, { useState, useEffect } from 'react'
 import InputField from '../../../global/atoms/InputField'
+import Button from '../../../global/atoms/Button'
+import ExamTypeOptions from '../../base/molecules/ExamTypeOptions'
 import PopupComponent from '../../../popups/PopupComponent'
 import SuggestionsList from '../../modify_questions/molecules/SuggestionsList'
-import { handleModifyForSearch, handleDeleteForSearch } from '../../helpers/formHandlers'
-import { dummyData } from '../../helpers/dummyData'
+import { handleSearch, handleSearchByCode } from '../../helpers/formHandlers'
 import '../../../../assets/styles/questions/find-question.css'
 
 const FindQuestion = ({ onQuestionFound, onResultsUpdate, lookingFor }) => {
   const [query, setQuery] = useState('') // state para almacenar la consulta (query) de busqueda
   const [errorMessage, setErrorMessage] = useState('') // state  mensajes de error
   const [suggestions, setSuggestions] = useState([]) // state para las sugerencias de busqueda
+  const [showAdvancedSearch, setShowAdvancedSearch] = useState(false) // state para mostrar la busqueda avanzada
+  const [searchCode, setSearchCode] = useState('') // state para almacenar el codigo de busqueda
+  const [searchExamType, setSearchExamType] = useState('both') // state para almacenar el tipo de examen de busqueda
 
   useEffect(() => {
-    // Este efecto se ejecuta cada vez que cambia 'query', 'questions' o 'onResultsUpdate'
-    if (query.length > 2) { // Solo realizar búsqueda si la consulta tiene más de 2 caracteres
-      if (lookingFor === 'delete') {
-        handleDeleteForSearch(query, onResultsUpdate)
-      } else {
-        handleModifyForSearch(query, setErrorMessage, setSuggestions)
-      }
-    } else {
-      onResultsUpdate ? onResultsUpdate(dummyData) : setSuggestions([]) // Limpiar las sugerencias si la consulta es menor a 3 caracteres
-    }
-  }, [query, onResultsUpdate, lookingFor])
+    handleSearch(query, setQuestions, searchExamType, setSearchCode, lookingFor)
+  }, [query, onResultsUpdate, lookingFor, searchExamType])
 
-  // Maneja el cambio en el campo de entrada
   const handleInputChange = (e) => {
     const searchTerm = e.target.value
-    setQuery(searchTerm) // Actualizar el estado de la consulta
+    setQuery(searchTerm)
+  }
+
+  const handleExamTypeChange = (e) => {
+    const examType = e.target.value
+    setSearchExamType(examType)
+  }
+
+  // interfaz para facilitar la busqueda // utilizar onResultsUpdate o setSuggestions
+  const setQuestions = (questions) => {
+    lookingFor === 'delete' ? onResultsUpdate(questions) : setSuggestions(questions)
   }
 
   // Maneja el clic en una sugerencia
@@ -37,6 +41,16 @@ const FindQuestion = ({ onQuestionFound, onResultsUpdate, lookingFor }) => {
     setSuggestions([]) // Limpiar las sugerencias
   }
 
+  const handleAdvancedSearch = () => {
+    setShowAdvancedSearch(!showAdvancedSearch)
+  }
+
+  const examTypeOptions = [
+    { value: 'both', label: 'Ambos' },
+    { value: 'academic', label: 'Académico' },
+    { value: 'dai', label: 'DAI' }
+  ]
+
   return (
     <div className='find-question-container'>
       {/* Mostrar mensaje de error si hay alguno */}
@@ -45,12 +59,34 @@ const FindQuestion = ({ onQuestionFound, onResultsUpdate, lookingFor }) => {
       )}
       {/* Campo de entrada para buscar preguntas */}
       <InputField
-        field={{ name: 'questionCode', label: 'Buscar Pregunta', type: 'text', placeholder: 'Ingrese el texto de la pregunta' }}
+        field={{ name: 'questionText', label: 'Buscar Pregunta', type: 'text', placeholder: 'Ingrese el texto de la pregunta' }}
         value={query}
         handleChange={handleInputChange}
         className='form-group'
       />
-      {/* Mostrar la lista de sugerencias si hay sugerencias y no se pasaron preguntas */}
+      {/* mostrar/ocultar la bsqueda avanzada */}
+      <Button type='button' className='btn btn-secondary' onClick={handleAdvancedSearch}>
+        {showAdvancedSearch ? 'Ocultar Búsqueda Avanzada' : 'Búsqueda Avanzada'}
+      </Button>
+
+      {/* busqueda avanzada */}
+      {showAdvancedSearch && (
+        <div className='advanced-search-container'>
+          <InputField
+            field={{ name: 'questionCode', label: 'Código de Pregunta', type: 'text', placeholder: 'Ingrese el código de la pregunta' }}
+            value={searchCode}
+            handleChange={(e) => handleSearchByCode(e, setQuery, setSearchCode, setSearchExamType, setQuestions)}
+            className='form-group'
+          />
+          <ExamTypeOptions
+            value={searchExamType}
+            handleChange={(e) => handleExamTypeChange(e)}
+            options={examTypeOptions}
+          />
+        </div>
+      )}
+
+      {/* lista de sugerencias cuando se esta en la pestana de modificar */}
       {suggestions.length > 0 && lookingFor !== 'delete' && (
         <SuggestionsList suggestions={suggestions} onSuggestionClick={handleSuggestionClick} />
       )}
