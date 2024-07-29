08ef9c8af9a313bbd9ae4e8e5a832944af352aac
Charly <carlosgarita3110@gmail.com>
2024-07-29 14:04:04 -0600

    feat: add SuggestionsList component for displaying suggestions
    This commit adds a new component called SuggestionsList, which is responsible for rendering a list of suggestions. It receives an array of suggestions as props and renders them as list items. Each suggestion item is clickable and triggers an onSuggestionClick event when clicked. This component will be used in the `modify_questions` module for displaying suggestions to the user.

---
+++ management_client/src/components/questions/modify_questions/molecules/SuggestionsList.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import '../../../../assets/styles/questions/suggestions-list.css'
+
+const SuggestionsList = ({ suggestions, onSuggestionClick }) => {
+  return (
+    <ul className='suggestions-list'>
+      {suggestions.map((item) => (
+        <li key={item.code} onClick={() => onSuggestionClick(item)}>
+          {item.question}
+        </li>
+      ))}
+    </ul>
+  )
+}
+
+export default SuggestionsList
