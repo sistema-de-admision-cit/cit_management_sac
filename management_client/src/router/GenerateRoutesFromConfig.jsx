d98e52e29cf8099bd6b8885c46b89196ef24f578
Charly <carlosgarita3110@gmail.com>
2024-08-07 14:14:49 -0600

    refactor: simplify route generation logic
    - Updated `generateRoutesFromConfig` to handle direct `items` input instead of nested `config` array.
    - Replaced `flatMap` with `map` for a more straightforward mapping of items and their children.

--- management_client/src/router/GenerateRoutesFromConfig.jsx
+++ management_client/src/router/GenerateRoutesFromConfig.jsx
@@ -1,31 +1,29 @@
 import HubViewComponent from '../components/hub/view/HubViewComponent'
 
 // Función para generar rutas dinámicas desde menuConfig
-const generateRoutesFromConfig = (config) => {
-  return config.flatMap((menuItem) =>
-    menuItem.items.map((item) => {
-      const children = item.subItems?.map((subItem) => ({
-        path: subItem.path,
-        element: subItem.component
-          ? <subItem.component />
-          : <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>{subItem.label}</div>
-      })) || []
+const generateRoutesFromConfig = (items) => {
+  return items.map((item) => {
+    const children = item.subItems?.map((subItem) => ({
+      path: subItem.path,
+      element: subItem.component
+        ? <subItem.component />
+        : <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>{subItem.label}</div>
+    })) || []
 
-      // Componente por defecto para las rutas padre
-      if (children.length > 0 && item.parentComponent) {
-        children.unshift({
-          index: true, // index true significa que es el componente principal (padre)
-          element: <item.parentComponent label={item.label} />
-        })
-      }
+    // Componente por defecto para las rutas padre
+    if (children.length > 0 && item.parentComponent) {
+      children.unshift({
+        index: true, // index true significa que es el componente principal (padre)
+        element: <item.parentComponent label={item.label} />
+      })
+    }
 
-      return {
-        path: item.path,
-        element: <HubViewComponent />,
-        children
-      }
-    })
-  )
+    return {
+      path: item.path,
+      element: <HubViewComponent />,
+      children
+    }
+  })
 }
 
 export default generateRoutesFromConfig
