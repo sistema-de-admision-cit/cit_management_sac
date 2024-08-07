00fe7e8cd7debc675536a9698f4d1f64ae30a756
Charly <carlosgarita3110@gmail.com>
2024-08-07 13:48:07 -0600

    feat: add Outlet component to HubViewComponent
    - Added <Outlet /> to render child routes within HubViewComponent.
    - Ensured currentPage is still passed to SideNavbar.

--- management_client/src/components/hub/view/HubViewComponent.jsx
+++ management_client/src/components/hub/view/HubViewComponent.jsx
@@ -1,11 +1,14 @@
 import SideNavbar from '../organisms/SideNavbar'
-import { useLocation } from 'react-router-dom'
+import { Outlet, useLocation } from 'react-router-dom'
 
 const HubViewComponent = () => {
   const location = useLocation()
   const currentPage = location.pathname
   return (
-    <SideNavbar currentPage={currentPage} />
+    <>
+      <SideNavbar currentPage={currentPage} />
+      <Outlet />
+    </>
   )
 }
 
