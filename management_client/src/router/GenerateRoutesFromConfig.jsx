118c67604409ccb685499c54c70924adfdfe9213
Jason Mora <jeisonmora0115@gmail.com>
2024-08-07 15:56:58 -0600

    refactor(router): enhance dynamic route generation with parent component support

--- management_client/src/router/GenerateRoutesFromConfig.jsx
+++ management_client/src/router/GenerateRoutesFromConfig.jsx
@@ -1,20 +1,25 @@
 import HubViewComponent from '../components/hub/view/HubViewComponent'
+import { ProtectedRoute } from './ProtectedRoute'
 
 // Función para generar rutas dinámicas desde menuConfig
 const generateRoutesFromConfig = (items) => {
   return items.map((item) => {
     const children = item.subItems?.map((subItem) => ({
       path: subItem.path,
-      element: subItem.component
-        ? <subItem.component />
-        : <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>{subItem.label}</div>
+      element: (
+        <ProtectedRoute>
+          {subItem.component
+            ? <subItem.component />
+            : <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>{subItem.label}</div>}
+        </ProtectedRoute>
+      )
     })) || []
 
     // Componente por defecto para las rutas padre
     if (item.parentComponent) {
       children.unshift({
         index: true, // index true significa que es el componente principal (padre)
-        element: <item.parentComponent label={item.label} />
+        element: <ProtectedRoute><item.parentComponent label={item.label} /></ProtectedRoute>
       })
     }
 
