077451369c94c3907f6df3b2564a0b4631704d43
Charly <carlosgarita3110@gmail.com>
2024-08-07 12:53:37 -0600

    feat: add AppRouter component for routing
    The `AppRouter` component has been added to handle routing in the management client application. This component uses `react-router-dom` to define routes for authentication, dynamic routes, and sub-routes. It improves the overall navigation and user experience of the application.

---
+++ management_client/src/router/AppRouter.jsx
@@ -0,0 +1,54 @@
+import React from 'react'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import HubViewComponent from '../components/hub/view/HubViewComponent'
+import menuConfig from '../components/hub/config/menuConfig'
+import LoginSection from '../components/auth/views/LoginSection'
+import RegisterSection from '../components/auth/views/RegisterSection'
+
+const AppRouter = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        {/* Rutas de autenticación */}
+        <Route path='/' element={<LoginSection sectionName='Iniciar Sesión' />} />
+        <Route path='/login' element={<LoginSection sectionName='Iniciar Sesión' />} />
+        <Route path='/register' element={<RegisterSection sectionName='Registrarse' />} />
+
+        {/* Rutas dinamicas */}
+        {menuConfig[0].items.map((item) => (
+          <React.Fragment key={item.key}>
+            <Route
+              path={item.path}
+              element={
+                <>
+                  <HubViewComponent />
+                  <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
+                    {item.label}
+                  </div>
+                </>
+              }
+            />
+            {item.subItems && item.subItems.map((subItem) => (
+              <Route
+                key={subItem.key}
+                path={subItem.path}
+                element={
+                  <>
+                    <HubViewComponent />
+                    {
+                      subItem.component
+                        ? <subItem.component />
+                        : <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>{subItem.label}</div>
+                    }
+                  </>
+                }
+              />
+            ))}
+          </React.Fragment>
+        ))}
+      </Routes>
+    </BrowserRouter>
+  )
+}
+
+export default AppRouter
