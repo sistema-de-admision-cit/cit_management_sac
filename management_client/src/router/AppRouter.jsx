c49b9325aaf78dcb3e851ef21e247b9c1bc4821b
Charly <carlosgarita3110@gmail.com>
2024-08-07 13:48:58 -0600

    feat: update AppRouter to include subItems in routes
    Refactor the AppRouter component to include subItems in the routes. This allows for rendering child routes within HubViewComponent. The update ensures compatibility with the latest version of react-router-dom and improves the routing functionality in the management client application.

--- management_client/src/router/AppRouter.jsx
+++ management_client/src/router/AppRouter.jsx
@@ -1,6 +1,6 @@
 // src/AppRouter.js
 import React from 'react'
-import { createBrowserRouter, RouterProvider, Outlet } from 'react-router-dom'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import HubViewComponent from '../components/hub/view/HubViewComponent'
 import menuConfig from '../components/hub/config/menuConfig'
 import LoginSection from '../components/auth/views/LoginSection'
@@ -12,30 +12,19 @@ const generateRoutesFromConfig = (config) => {
 
   config.forEach((menuItem) => {
     menuItem.items.forEach((item) => {
+      const children = item.subItems?.map((subItem) => ({
+        path: subItem.path,
+        element: subItem.component ? <subItem.component /> : <div>{subItem.label}</div>
+      })) || []
+
       routes.push({
         path: item.path,
         element: (
           <>
             <HubViewComponent />
-            <Outlet />
           </>
         ),
-        children: item.subItems?.map((subItem) => ({
-          path: subItem.path,
-          element: (
-            <>
-              {
-                subItem.component
-                  ? <subItem.component />
-                  : (
-                    <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
-                      {subItem.label}
-                    </div>
-                    )
-              }
-            </>
-          )
-        })) || []
+        children
       })
     })
   })
