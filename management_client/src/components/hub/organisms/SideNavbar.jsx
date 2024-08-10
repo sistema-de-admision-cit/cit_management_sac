393694b924ce423bf67493ba371cf3ac965220a1
Charly <carlosgarita3110@gmail.com>
2024-08-10 17:08:58 -0600

    refactor: update SideNavbar to use logout function from useAuth hook

--- management_client/src/components/hub/organisms/SideNavbar.jsx
+++ management_client/src/components/hub/organisms/SideNavbar.jsx
@@ -2,11 +2,16 @@ import React from 'react'
 import NavGroup from '../molecules/NavGroup'
 
 import '../../../assets/styles/hub/side-navbar.css'
+import { useAuth } from '../../../router/AuthProvider'
 
 const SideNavbar = ({ currentPage }) => {
+  const { logout } = useAuth()
   return (
     <ul className='nav'>
-      <NavGroup currentPage={currentPage} />
+      <NavGroup
+        currentPage={currentPage}
+        logout={logout}
+      />
     </ul>
   )
 }
