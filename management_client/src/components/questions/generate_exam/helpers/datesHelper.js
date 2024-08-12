5680e8bcb8c1f672b7ed045ed64eeb0aab29deb1
Charly <carlosgarita3110@gmail.com>
2024-08-12 14:49:54 -0600

    feat(helpers): add getNearestAvailableDate function

--- management_client/src/components/questions/generate_exam/helpers/datesHelper.js
+++ management_client/src/components/questions/generate_exam/helpers/datesHelper.js
@@ -4,3 +4,11 @@ export const availableDates = (date) => {
   const day = date.getDay()
   return availableDays.includes(day)
 }
+
+export const getNearestAvailableDate = (date) => {
+  let newDate = new Date(date)
+  while (!availableDates(newDate)) {
+    newDate = new Date(newDate.setDate(newDate.getDate() + 1))
+  }
+  return newDate
+}
