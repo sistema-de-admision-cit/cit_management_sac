b2e6403dad95a9628c2a74472191192a416d2696
Charly <carlosgarita3110@gmail.com>
2024-08-12 16:48:40 -0600

    feat: add questionFormOptions for exam and question types
    - Added `questionFormOptions.js` to centralize the definition of exam and question type options.

---
+++ management_client/src/components/questions/base/helpers/questionFormOptions.js
@@ -0,0 +1,13 @@
+export const EXAM_TYPE_OPTIONS = [
+  { value: 'academic', label: 'Académico' },
+  { value: 'dai', label: 'DAI' }
+]
+
+export const QUESTION_TYPE_OPTIONS = {
+  dai: [
+    { value: 'short', label: 'Respuesta Corta' }
+  ],
+  academic: [
+    { value: 'unique', label: 'Selección Única' }
+  ]
+}
