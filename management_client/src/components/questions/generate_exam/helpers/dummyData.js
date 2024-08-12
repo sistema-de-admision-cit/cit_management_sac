063824786ad305de2030090931aad20190c6ddb8
Charly <carlosgarita3110@gmail.com>
2024-08-12 09:51:18 -0600

    feat: add dummy data

---
+++ management_client/src/components/questions/generate_exam/helpers/dummyData.js
@@ -0,0 +1,503 @@
+const questionsExample = [
+  {
+    questionText: 'Lorem ipsum dolor sit amet?',
+    answers: [
+      'Consectetur adipiscing elit',
+      'Sed do eiusmod tempor',
+      'CORRECT ANSWER',
+      'Magna aliqua'
+    ],
+    correctAnswer: 'CORRECT ANSWER'
+  },
+  {
+    questionText: 'Ut enim ad minim veniam?',
+    answers: [
+      'CORRECT ANSWER',
+      'Ullamco laboris nisi ut aliquip',
+      'Ex ea commodo consequat',
+      'Duis aute irure dolor'
+    ],
+    correctAnswer: 'CORRECT ANSWER'
+  },
+  {
+    questionText: 'Duis aute irure dolor in reprehenderit?',
+    answers: [
+      'In voluptate velit esse',
+      'CORRECT ANSWER',
+      'Pariatur',
+      'Excepteur sint occaecat cupidatat'
+    ],
+    correctAnswer: 'CORRECT ANSWER'
+  },
+  {
+    questionText: 'Excepteur sint occaecat cupidatat non proident?',
+    answers: [
+      'Sunt in culpa qui officia',
+      'CORRECT ANSWER',
+      'Sed ut perspiciatis unde omnis iste',
+      'Natus error sit voluptatem'
+    ],
+    correctAnswer: 'CORRECT ANSWER'
+  },
+  {
+    questionText: 'Nemo enim ipsam voluptatem quia?',
+    answers: [
+      'Voluptas sit aspernatur',
+      'Aut odit aut fugit',
+      'CORRECT ANSWER',
+      'Dolores eos qui ratione'
+    ],
+    correctAnswer: 'CORRECT ANSWER'
+  },
+  {
+    questionText: 'Neque porro quisquam est?',
+    answers: [
+      'Qui dolorem ipsum quia dolor',
+      'CORRECT ANSWER',
+      'Sit amet',
+      'Consectetur adipiscing elit'
+    ],
+    correctAnswer: 'CORRECT ANSWER'
+  },
+  {
+    questionText: 'Sed quia non numquam eius modi tempora incidunt?',
+    answers: [
+      'Ut labore et dolore magnam aliquam quaerat',
+      'CORRECT ANSWER',
+      'Voluptatem',
+      'Ut enim ad minima veniam'
+    ],
+    correctAnswer: 'CORRECT ANSWER'
+  },
+  {
+    questionText: 'Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam?',
+    answers: [
+      'CORRECT ANSWER',
+      'Fugiat nulla pariatur',
+      'Excepteur sint occaecat cupidatat non proident',
+      'Sunt in culpa qui officia deserunt mollit anim id est laborum'
+    ],
+    correctAnswer: 'CORRECT ANSWER'
+  }, {
+    questionText: 'Lorem ipsum dolor sit amet?',
+    answers: [
+      'Consectetur adipiscing elit',
+      'Sed do eiusmod tempor',
+      'CORRECT ANSWER',
+      'Magna aliqua'
+    ],
+    correctAnswer: 'CORRECT ANSWER'
+  },
+  {
+    questionText: 'Ut enim ad minim veniam?',
+    answers: [
+      'CORRECT ANSWER',
+      'Ullamco laboris nisi ut aliquip',
+      'Ex ea commodo consequat',
+      'Duis aute irure dolor'
+    ],
+    correctAnswer: 'CORRECT ANSWER'
+  },
+  {
+    questionText: 'Duis aute irure dolor in reprehenderit?',
+    answers: [
+      'In voluptate velit esse',
+      'CORRECT ANSWER',
+      'Pariatur',
+      'Excepteur sint occaecat cupidatat'
+    ],
+    correctAnswer: 'CORRECT ANSWER'
+  },
+  {
+    questionText: 'Excepteur sint occaecat cupidatat non proident?',
+    answers: [
+      'Sunt in culpa qui officia',
+      'CORRECT ANSWER',
+      'Sed ut perspiciatis unde omnis iste',
+      'Natus error sit voluptatem'
+    ],
+    correctAnswer: 'CORRECT ANSWER'
+  },
+  {
+    questionText: 'Nemo enim ipsam voluptatem quia?',
+    answers: [
+      'Voluptas sit aspernatur',
+      'Aut odit aut fugit',
+      'CORRECT ANSWER',
+      'Dolores eos qui ratione'
+    ],
+    correctAnswer: 'CORRECT ANSWER'
+  },
+  {
+    questionText: 'Neque porro quisquam est?',
+    answers: [
+      'Qui dolorem ipsum quia dolor',
+      'CORRECT ANSWER',
+      'Sit amet',
+      'Consectetur adipiscing elit'
+    ],
+    correctAnswer: 'CORRECT ANSWER'
+  },
+  {
+    questionText: 'Sed quia non numquam eius modi tempora incidunt?',
+    answers: [
+      'Ut labore et dolore magnam aliquam quaerat',
+      'CORRECT ANSWER',
+      'Voluptatem',
+      'Ut enim ad minima veniam'
+    ],
+    correctAnswer: 'CORRECT ANSWER'
+  },
+  {
+    questionText: 'Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam?',
+    answers: [
+      'CORRECT ANSWER',
+      'Fugiat nulla pariatur',
+      'Excepteur sint occaecat cupidatat non proident',
+      'Sunt in culpa qui officia deserunt mollit anim id est laborum'
+    ],
+    correctAnswer: 'CORRECT ANSWER'
+  },
+  {
+    questionText: 'Lorem ipsum dolor sit amet?',
+    answers: [
+      'Consectetur adipiscing elit',
+      'Sed do eiusmod tempor',
+      'CORRECT ANSWER',
+      'Magna aliqua'
+    ],
+    correctAnswer: 'CORRECT ANSWER'
+  },
+  {
+    questionText: 'Ut enim ad minim veniam?',
+    answers: [
+      'CORRECT ANSWER',
+      'Ullamco laboris nisi ut aliquip',
+      'Ex ea commodo consequat',
+      'Duis aute irure dolor'
+    ],
+    correctAnswer: 'CORRECT ANSWER'
+  },
+  {
+    questionText: 'Duis aute irure dolor in reprehenderit?',
+    answers: [
+      'In voluptate velit esse',
+      'CORRECT ANSWER',
+      'Pariatur',
+      'Excepteur sint occaecat cupidatat'
+    ],
+    correctAnswer: 'CORRECT ANSWER'
+  },
+  {
+    questionText: 'Excepteur sint occaecat cupidatat non proident?',
+    answers: [
+      'Sunt in culpa qui officia',
+      'CORRECT ANSWER',
+      'Sed ut perspiciatis unde omnis iste',
+      'Natus error sit voluptatem'
+    ],
+    correctAnswer: 'CORRECT ANSWER'
+  },
+  {
+    questionText: 'Nemo enim ipsam voluptatem quia?',
+    answers: [
+      'Voluptas sit aspernatur',
+      'Aut odit aut fugit',
+      'CORRECT ANSWER',
+      'Dolores eos qui ratione'
+    ],
+    correctAnswer: 'CORRECT ANSWER'
+  },
+  {
+    questionText: 'Neque porro quisquam est?',
+    answers: [
+      'Qui dolorem ipsum quia dolor',
+      'CORRECT ANSWER',
+      'Sit amet',
+      'Consectetur adipiscing elit'
+    ],
+    correctAnswer: 'CORRECT ANSWER'
+  },
+  {
+    questionText: 'Sed quia non numquam eius modi tempora incidunt?',
+    answers: [
+      'Ut labore et dolore magnam aliquam quaerat',
+      'CORRECT ANSWER',
+      'Voluptatem',
+      'Ut enim ad minima veniam'
+    ],
+    correctAnswer: 'CORRECT ANSWER'
+  },
+  {
+    questionText: 'Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam?',
+    answers: [
+      'CORRECT ANSWER',
+      'Fugiat nulla pariatur',
+      'Excepteur sint occaecat cupidatat non proident',
+      'Sunt in culpa qui officia deserunt mollit anim id est laborum'
+    ],
+    correctAnswer: 'CORRECT ANSWER'
+  },
+  {
+    questionText: 'Lorem ipsum dolor sit amet?',
+    answers: [
+      'Consectetur adipiscing elit',
+      'Sed do eiusmod tempor',
+      'CORRECT ANSWER',
+      'Magna aliqua'
+    ],
+    correctAnswer: 'CORRECT ANSWER'
+  },
+  {
+    questionText: 'Ut enim ad minim veniam?',
+    answers: [
+      'CORRECT ANSWER',
+      'Ullamco laboris nisi ut aliquip',
+      'Ex ea commodo consequat',
+      'Duis aute irure dolor'
+    ],
+    correctAnswer: 'CORRECT ANSWER'
+  },
+  {
+    questionText: 'Duis aute irure dolor in reprehenderit?',
+    answers: [
+      'In voluptate velit esse',
+      'CORRECT ANSWER',
+      'Pariatur',
+      'Excepteur sint occaecat cupidatat'
+    ],
+    correctAnswer: 'CORRECT ANSWER'
+  },
+  {
+    questionText: 'Excepteur sint occaecat cupidatat non proident?',
+    answers: [
+      'Sunt in culpa qui officia',
+      'CORRECT ANSWER',
+      'Sed ut perspiciatis unde omnis iste',
+      'Natus error sit voluptatem'
+    ],
+    correctAnswer: 'CORRECT ANSWER'
+  },
+  {
+    questionText: 'Ut enim ad minim veniam?',
+    answers: [
+      'CORRECT ANSWER',
+      'Ullamco laboris nisi ut aliquip',
+      'Ex ea commodo consequat',
+      'Duis aute irure dolor'
+    ],
+    correctAnswer: 'CORRECT ANSWER'
+  },
+  {
+    questionText: 'Duis aute irure dolor in reprehenderit?',
+    answers: [
+      'In voluptate velit esse',
+      'CORRECT ANSWER',
+      'Pariatur',
+      'Excepteur sint occaecat cupidatat'
+    ],
+    correctAnswer: 'CORRECT ANSWER'
+  },
+  {
+    questionText: 'Excepteur sint occaecat cupidatat non proident?',
+    answers: [
+      'Sunt in culpa qui officia',
+      'CORRECT ANSWER',
+      'Sed ut perspiciatis unde omnis iste',
+      'Natus error sit voluptatem'
+    ],
+    correctAnswer: 'CORRECT ANSWER'
+  },
+  {
+    questionText: 'Nemo enim ipsam voluptatem quia?',
+    answers: [
+      'Voluptas sit aspernatur',
+      'Aut odit aut fugit',
+      'CORRECT ANSWER',
+      'Dolores eos qui ratione'
+    ],
+    correctAnswer: 'CORRECT ANSWER'
+  },
+  {
+    questionText: 'Neque porro quisquam est?',
+    answers: [
+      'Qui dolorem ipsum quia dolor',
+      'CORRECT ANSWER',
+      'Sit amet',
+      'Consectetur adipiscing elit'
+    ],
+    correctAnswer: 'CORRECT ANSWER'
+  },
+  {
+    questionText: 'Sed quia non numquam eius modi tempora incidunt?',
+    answers: [
+      'Ut labore et dolore magnam aliquam quaerat',
+      'CORRECT ANSWER',
+      'Voluptatem',
+      'Ut enim ad minima veniam'
+    ],
+    correctAnswer: 'CORRECT ANSWER'
+  },
+  {
+    questionText: 'Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam?',
+    answers: [
+      'CORRECT ANSWER',
+      'Fugiat nulla pariatur',
+      'Excepteur sint occaecat cupidatat non proident',
+      'Sunt in culpa qui officia deserunt mollit anim id est laborum'
+    ],
+    correctAnswer: 'CORRECT ANSWER'
+  },
+  {
+    questionText: 'Lorem ipsum dolor sit amet?',
+    answers: [
+      'Consectetur adipiscing elit',
+      'Sed do eiusmod tempor',
+      'CORRECT ANSWER',
+      'Magna aliqua'
+    ],
+    correctAnswer: 'CORRECT ANSWER'
+  },
+  {
+    questionText: 'Ut enim ad minim veniam?',
+    answers: [
+      'CORRECT ANSWER',
+      'Ullamco laboris nisi ut aliquip',
+      'Ex ea commodo consequat',
+      'Duis aute irure dolor'
+    ],
+    correctAnswer: 'CORRECT ANSWER'
+  },
+  {
+    questionText: 'Duis aute irure dolor in reprehenderit?',
+    answers: [
+      'In voluptate velit esse',
+      'CORRECT ANSWER',
+      'Pariatur',
+      'Excepteur sint occaecat cupidatat'
+    ],
+    correctAnswer: 'CORRECT ANSWER'
+  },
+  {
+    questionText: 'Excepteur sint occaecat cupidatat non proident?',
+    answers: [
+      'Sunt in culpa qui officia',
+      'CORRECT ANSWER',
+      'Sed ut perspiciatis unde omnis iste',
+      'Natus error sit voluptatem'
+    ],
+    correctAnswer: 'CORRECT ANSWER'
+  },
+  {
+    questionText: 'Nemo enim ipsam voluptatem quia?',
+    answers: [
+      'Voluptas sit aspernatur',
+      'Aut odit aut fugit',
+      'CORRECT ANSWER',
+      'Dolores eos qui ratione'
+    ],
+    correctAnswer: 'CORRECT ANSWER'
+  },
+  {
+    questionText: 'Neque porro quisquam est?',
+    answers: [
+      'Qui dolorem ipsum quia dolor',
+      'CORRECT ANSWER',
+      'Sit amet',
+      'Consectetur adipiscing elit'
+    ],
+    correctAnswer: 'CORRECT ANSWER'
+  },
+  {
+    questionText: 'Sed quia non numquam eius modi tempora incidunt?',
+    answers: [
+      'Ut labore et dolore magnam aliquam quaerat',
+      'CORRECT ANSWER',
+      'Voluptatem',
+      'Ut enim ad minima veniam'
+    ],
+    correctAnswer: 'CORRECT ANSWER'
+  },
+  {
+    questionText: 'Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam?',
+    answers: [
+      'CORRECT ANSWER',
+      'Fugiat nulla pariatur',
+      'Excepteur sint occaecat cupidatat non proident',
+      'Sunt in culpa qui officia deserunt mollit anim id est laborum'
+    ],
+    correctAnswer: 'CORRECT ANSWER'
+  },
+  {
+    questionText: 'Lorem ipsum dolor sit amet?',
+    answers: [
+      'Consectetur adipiscing elit',
+      'Sed do eiusmod tempor',
+      'CORRECT ANSWER',
+      'Magna aliqua'
+    ],
+    correctAnswer: 'CORRECT ANSWER'
+  },
+  {
+    questionText: 'Ut enim ad minim veniam?',
+    answers: [
+      'CORRECT ANSWER',
+      'Ullamco laboris nisi ut aliquip',
+      'Ex ea commodo consequat',
+      'Duis aute irure dolor'
+    ],
+    correctAnswer: 'CORRECT ANSWER'
+  },
+  {
+    questionText: 'Duis aute irure dolor in reprehenderit?',
+    answers: [
+      'In voluptate velit esse',
+      'CORRECT ANSWER',
+      'Pariatur',
+      'Excepteur sint occaecat cupidatat'
+    ],
+    correctAnswer: 'CORRECT ANSWER'
+  },
+  {
+    questionText: 'Excepteur sint occaecat cupidatat non proident?',
+    answers: [
+      'Sunt in culpa qui officia',
+      'CORRECT ANSWER',
+      'Sed ut perspiciatis unde omnis iste',
+      'Natus error sit voluptatem'
+    ],
+    correctAnswer: 'CORRECT ANSWER'
+  },
+  {
+    questionText: 'Nemo enim ipsam voluptatem quia?',
+    answers: [
+      'Voluptas sit aspernatur',
+      'Aut odit aut fugit',
+      'CORRECT ANSWER',
+      'Dolores eos qui ratione'
+    ],
+    correctAnswer: 'CORRECT ANSWER'
+  },
+  {
+    questionText: 'Neque porro quisquam est?',
+    answers: [
+      'Qui dolorem ipsum quia dolor',
+      'CORRECT ANSWER',
+      'Sit amet',
+      'Consectetur adipiscing elit'
+    ],
+    correctAnswer: 'CORRECT ANSWER'
+  },
+  {
+    questionText: 'Sed quia non numquam eius modi tempora incidunt?',
+    answers: [
+      'Ut labore et dolore magnam aliquam quaerat',
+      'CORRECT ANSWER',
+      'Voluptatem',
+      'Ut enim ad minima veniam'
+    ],
+    correctAnswer: 'CORRECT ANSWER'
+  }
+]
+
+export default questionsExample
