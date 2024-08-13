382c897adf0607b8b7873249f67652363443e5eb
Charly <carlosgarita3110@gmail.com>
2024-08-12 20:40:37 -0600

    refactor: rename `AuthContent` to `LoginContent` and remove registration logic

---
+++ management_client/src/components/auth/organisms/LoginContent.jsx
@@ -0,0 +1,26 @@
+import Form from '../molecules/Form'
+
+import '../../../assets/styles/auth/right-column.css'
+import '../../../assets/styles/auth/links.css'
+
+const LoginContent = ({ fields, formData, handleChange, onSubmit }) => {
+  return (
+    <div className='right-column'>
+      <div className='form'>
+        <h2>Iniciar Sesión</h2>
+        <Form
+          fields={fields}
+          formData={formData}
+          handleChange={handleChange}
+          onSubmit={onSubmit}
+          sectionName='Iniciar Sesión'
+        />
+        <div className='copyright'>
+          <p>Copyright &copy; 2024. Todos los derechos reservados.</p>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default LoginContent
