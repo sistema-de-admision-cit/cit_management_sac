3cdf1fc673019f611ab197def7e910ff2fab693f
Jason Mora <jeisonmora0115@gmail.com>
2024-08-14 15:10:33 -0600

    feat: add spring base project

---
+++ cit-sac-back-end/src/main/java/cr/co/ctpcit/sac/CitSacBackEndApplication.java
@@ -0,0 +1,13 @@
+package cr.co.ctpcit.sac;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CitSacBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CitSacBackEndApplication.class, args);
+	}
+
+}
---
+++ cit-sac-back-end/src/test/java/cr/co/ctpcit/sac/CitSacBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package cr.co.ctpcit.sac;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CitSacBackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
