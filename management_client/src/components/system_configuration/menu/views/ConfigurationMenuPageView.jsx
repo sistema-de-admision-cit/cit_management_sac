4d1b8046af7afaa9f62c8ff69a6e528fc5b82148
Charly <carlosgarita3110@gmail.com>
2024-08-19 14:35:27 -0600

    feat: add ConfigurationMenuPageView component
    The ConfigurationMenuPageView component is added to display the configuration menu page. It uses the MenuPage component to render the menu items and their descriptions from the menuConfig.

---
+++ management_client/src/components/system_configuration/menu/views/ConfigurationMenuPageView.jsx
@@ -0,0 +1,16 @@
+import MenuPage from '../../../menu_overview/templates/MenuPage'
+import menuConfig from '../../../hub/config/menuConfig'
+
+const ConfigurationMenuPageView = () => {
+  const menuExamsSection = menuConfig[0].items.find(item => item.key === 'configurations')
+
+  return (
+    <MenuPage
+      menuTitle={menuExamsSection.label}
+      menuDescription={menuExamsSection.description}
+      menuItems={menuExamsSection.subItems}
+    />
+  )
+}
+
+export default ConfigurationMenuPageView
