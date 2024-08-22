d8fd0649fe646c6c6acf808dda530f2774f476eb
Charly <carlosgarita3110@gmail.com>
2024-08-21 21:57:01 -0600

    feat: add ApplicationDaysSelector component
    - Created `ApplicationDaysSelector` component to manage the selection of application days (Monday to Friday).

---
+++ management_client/src/components/modules/system_configuration/exam_schedule/molecules/ApplicationDaysSelector.jsx
@@ -0,0 +1,17 @@
+import InputField from '../../../../core/global/atoms/InputField'
+
+const ApplicationDaysSelector = ({ selectedDays, onDayChange }) => (
+  <div className='days'>
+    {['Lunes', 'Martes', 'Miércoles', 'Jueves', 'Viernes'].map((day) => (
+      <div key={day}>
+        <InputField
+          field={{ type: 'checkbox', name: day, label: day }}
+          value={selectedDays.includes(day)}
+          handleChange={() => onDayChange(day)}
+        />
+      </div>
+    ))}
+  </div>
+)
+
+export default ApplicationDaysSelector
