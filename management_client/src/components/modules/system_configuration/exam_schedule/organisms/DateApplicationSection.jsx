910e280c90dc72ccf4929dc0ae157815a5677c97
Charly <carlosgarita3110@gmail.com>
2024-08-21 22:03:08 -0600

    feat: add DateApplicationSection organism
    - Created `DateApplicationSection` organism to manage the application date configuration.
    - Incorporated `InputField` for the "all year" checkbox and `DateRangePicker` for selecting a date range.
    - Passed `allYear`, `startDate`, `endDate`, and respective change handlers as props to manage the state and interactions.

---
+++ management_client/src/components/modules/system_configuration/exam_schedule/organisms/DateApplicationSection.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import DateRangePicker from '../molecules/DateRangePicker'
+import InputField from '../../../../core/global/atoms/InputField'
+
+const DateApplicationSection = ({ allYear, startDate, endDate, onAllYearChange, onStartDateChange, onEndDateChange }) => (
+  <div className='application-dates'>
+    <h2>Fechas de Aplicación <span className='required'>*</span></h2>
+    <InputField
+      field={{ type: 'checkbox', name: 'allYear', label: 'Todo el año' }}
+      value={allYear}
+      handleChange={onAllYearChange}
+    />
+    {!allYear && (
+      <DateRangePicker
+        startDate={startDate}
+        endDate={endDate}
+        onStartDateChange={onStartDateChange}
+        onEndDateChange={onEndDateChange}
+      />
+    )}
+  </div>
+)
+
+export default DateApplicationSection
