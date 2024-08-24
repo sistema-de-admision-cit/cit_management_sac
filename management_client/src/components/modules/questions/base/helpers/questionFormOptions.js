f04aac579372d0ddaef4dd34229e5fb04faae155
Charly <carlosgarita3110@gmail.com>
2024-08-24 08:37:26 -0600

    refactor(questions): remove unused QUESTION_TYPE_OPTIONS constant
    - Removed the `QUESTION_TYPE_OPTIONS` export as it is no longer utilized in the application.

--- management_client/src/components/modules/questions/base/helpers/questionFormOptions.js
+++ management_client/src/components/modules/questions/base/helpers/questionFormOptions.js
@@ -2,12 +2,3 @@ export const EXAM_TYPE_OPTIONS = [
   { value: 'academic', label: 'Académico' },
   { value: 'dai', label: 'DAI' }
 ]
-
-export const QUESTION_TYPE_OPTIONS = {
-  dai: [
-    { value: 'short', label: 'Respuesta Corta' }
-  ],
-  academic: [
-    { value: 'unique', label: 'Selección Única' }
-  ]
-}
