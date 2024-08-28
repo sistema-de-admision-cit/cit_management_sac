7fff29349e0f31655c143ed42f9be4924f0812b6
Charly <carlosgarita3110@gmail.com>
2024-08-28 15:18:11 -0600

    feat: add PercentagesForm component for system configuration
    The commit adds a new file `PercentagesForm.jsx` under the `system_configuration/percentages/molecules` directory. The `PercentagesForm` component is a form that allows users to input and save percentage values for different exams.

---
+++ management_client/src/components/modules/system_configuration/percentages/molecules/PercentagesForm.jsx
@@ -0,0 +1,29 @@
+// components/questions/molecules/PercentagesForm.jsx
+import InputField from '../../../../core/global/atoms/InputField'
+import Button from '../../../../core/global/atoms/Button'
+
+const PercentagesForm = ({ formValues, handleChange }) => (
+  <form onSubmit={(e) => e.preventDefault()}>
+    <InputField
+      field={{ name: 'academicExam', label: 'Académico', type: 'text', placeholder: '20', required: true }}
+      value={formValues.academicExam}
+      handleChange={(e) => handleChange('academicExam', e.target.value)}
+      className='form-group'
+    />
+    <InputField
+      field={{ name: 'daiExam', label: 'Psicológico', type: 'text', placeholder: '20', required: true }}
+      value={formValues.daiExam}
+      handleChange={(e) => handleChange('daiExam', e.target.value)}
+      className='form-group'
+    />
+    <InputField
+      field={{ name: 'englishExam', label: 'Inglés', type: 'text', placeholder: '20', required: true }}
+      value={formValues.englishExam}
+      handleChange={(e) => handleChange('englishExam', e.target.value)}
+      className='form-group'
+    />
+    <Button className='btn btn-primary' onClick={() => console.log('Save')}>Guardar</Button>
+  </form>
+)
+
+export default PercentagesForm
