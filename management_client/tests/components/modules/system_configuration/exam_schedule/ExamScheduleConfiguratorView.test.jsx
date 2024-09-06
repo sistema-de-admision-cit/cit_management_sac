ba7ac2dfb60e094f7f0c2c6a92dcab248060d027
Charly <carlosgarita3110@gmail.com>
2024-09-05 19:10:34 -0600

    test(exam-schedule-configurator): add form submission test for ExamScheduleConfiguratorView component
    - Added a new test case for handling form submission when the form is valid.
    - Simulated user interactions like selecting dates, application days, and times.
    - Verified success message rendering after form submission.
    - Included `afterEach` cleanup to reset DOM after each test.

--- management_client/tests/components/modules/system_configuration/exam_schedule/ExamScheduleConfiguratorView.test.jsx
+++ management_client/tests/components/modules/system_configuration/exam_schedule/ExamScheduleConfiguratorView.test.jsx
@@ -1,9 +1,13 @@
-import { it, expect, describe } from 'vitest'
-import { render, screen } from '@testing-library/react'
+import { it, expect, describe, afterEach } from 'vitest'
+import { render, screen, fireEvent, cleanup, waitFor } from '@testing-library/react'
 import ExamScheduleConfiguratorView from '../../../../../src/components/modules/system_configuration/exam_schedule/view/ExamScheduleConfiguratorView'
 import '@testing-library/jest-dom/vitest'
 
 describe('ExamScheduleConfiguratorView', () => {
+  afterEach(() => {
+    cleanup()
+  })
+
   it('debería renderizar el formulario correctamente', () => {
     render(<ExamScheduleConfiguratorView />)
 
@@ -18,5 +22,30 @@ describe('ExamScheduleConfiguratorView', () => {
     // Verificar la presencia de botones
     expect(screen.getByRole('button', { name: /Guardar/i })).toBeInTheDocument()
     expect(screen.getByRole('button', { name: /Cancelar/i })).toBeInTheDocument()
+  })
+
+  it('debería manejar el envío del formulario correctamente cuando el formulario es válido', async () => {
+    render(<ExamScheduleConfiguratorView />)
+
+    // Simular la selección de fechas de inicio y fin
+    const allYearCheckBox = screen.getByText(/Todo el año/i, { selector: 'label' }).nextElementSibling
+    fireEvent.click(allYearCheckBox)
+
+    // Simular la selección de días de aplicación
+    const mondayCheckBox = screen.getByText(/lunes/i, { selector: 'label' }).nextElementSibling
+    fireEvent.click(mondayCheckBox)
+
+    // Simular la selección de hora de aplicación
+    const startTimeInput = screen.getByText(/Hora de Aplicación/i).nextElementSibling.querySelector('input')
+    fireEvent.change(startTimeInput, { target: { value: '08:00' } })
+
+    // Simular el envío del formulario
+    const saveButton = screen.getByRole('button', { name: /Guardar/i })
+    fireEvent.click(saveButton)
+
+    // Verificar que el mensaje de éxito se muestra
+    await waitFor(() => {
+      expect(screen.getByText(/Guardando.../i)).toBeInTheDocument()
+    })
   })
 })
