7f470412e601a10aa3281c0dfbe655674ace7cb8
moraja1 <jeisonmora0115@gmail.com>
2024-09-06 16:02:49 -0600

    chore: add entities for inscription process, Spring is running

---
+++ cit-sac-back-end/src/main/java/cr/co/ctpcit/citsacbackend/data/entities/DocumentEntity.java
@@ -0,0 +1,38 @@
+package cr.co.ctpcit.citsacbackend.data.entities;
+
+import cr.co.ctpcit.citsacbackend.data.enums.DocType;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.*;
+
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@ToString
+@Entity
+@Table(name = "tbl_documents")
+public class DocumentEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "document_id", nullable = false)
+    private Integer id;
+
+    @NotNull
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "enrollment_id", nullable = false)
+    private EnrollmentEntity enrollment;
+
+    @NotNull
+    @Enumerated(EnumType.STRING)
+    @Column(name = "document_type", nullable = false)
+    private DocType documentType;
+
+    @Size(max = 255)
+    @NotNull
+    @Column(name = "document_url", nullable = false)
+    private String documentUrl;
+
+}
---
+++ cit-sac-back-end/src/main/java/cr/co/ctpcit/citsacbackend/data/entities/ParentsGuardianEntity.java
@@ -0,0 +1,68 @@
+package cr.co.ctpcit.citsacbackend.data.entities;
+
+import cr.co.ctpcit.citsacbackend.data.enums.IdType;
+import cr.co.ctpcit.citsacbackend.data.enums.Relationship;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.*;
+
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@ToString
+@Entity
+@Table(name = "tbl_parentsguardians")
+public class ParentsGuardianEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "parent_guardian_id", nullable = false)
+    private Integer id;
+
+    @Size(max = 32)
+    @NotNull
+    @Column(name = "first_name", nullable = false, length = 32)
+    private String firstName;
+
+    @Size(max = 32)
+    @NotNull
+    @Column(name = "first_surname", nullable = false, length = 32)
+    private String firstSurname;
+
+    @Size(max = 32)
+    @Column(name = "second_surname", length = 32)
+    private String secondSurname;
+
+    @NotNull
+    @Enumerated(EnumType.STRING)
+    @Column(name = "id_type", nullable = false)
+    private IdType idType;
+
+    @Size(max = 20)
+    @NotNull
+    @Column(name = "id_number", nullable = false, length = 20)
+    private String idNumber;
+
+    @Size(max = 20)
+    @NotNull
+    @Column(name = "phone_number", nullable = false, length = 20)
+    private String phoneNumber;
+
+    @Size(max = 100)
+    @NotNull
+    @Column(name = "email", nullable = false, length = 100)
+    private String email;
+
+    @Size(max = 100)
+    @NotNull
+    @Column(name = "home_address", nullable = false, length = 100)
+    private String homeAddress;
+
+    @NotNull
+    @Enumerated(EnumType.STRING)
+    @Column(name = "relationship", nullable = false)
+    private Relationship relationship;
+
+}
