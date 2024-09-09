1057cf09b3038b994451de150960949981843185
Charly <carlosgarita3110@gmail.com>
2024-09-09 15:40:42 -0600

    feat: enhance PercentagesConfiguratorView with message handling and save functionality
    - Added useMessages hook for displaying success and error messages.
    - Introduced loading state to handle form submission status.
    - Implemented handleSave function to simulate API call and update messages based on result.
    - Passed onSave and loading props to PercentagesForm for better integration.

--- management_client/src/components/modules/system_configuration/percentages/view/PercentagesConfiguratorView.jsx
+++ management_client/src/components/modules/system_configuration/percentages/view/PercentagesConfiguratorView.jsx
@@ -1,8 +1,12 @@
 import { useState } from 'react'
 import SectionLayout from '../../../../core/global/molecules/SectionLayout'
 import PercentagesForm from '../molecules/PercentagesForm'
+import useMessages from '../../../../core/global/hooks/useMessages'
 
 const PercentagesConfiguratorView = () => {
+  const { setErrorMessage, setSuccessMessage, renderMessages } = useMessages()
+  const [loading, setLoading] = useState(false)
+  // TODO: Fetch data from API
   const [formValues, setFormValues] = useState({
     academicExam: 0,
     daiExam: 0,
@@ -16,13 +20,31 @@ const PercentagesConfiguratorView = () => {
     })
   }
 
+  const handleSave = async () => {
+    setLoading(true)
+    await new Promise((resolve) => setTimeout(resolve, 1000))
+    try {
+      setSuccessMessage('Porcentajes guardados correctamente.')
+    } catch (error) {
+      setErrorMessage('Error al guardar los porcentajes.')
+    } finally {
+      setLoading(false)
+    }
+  }
+
   return (
     <SectionLayout title='Configurar Porcentajes'>
       <div className='container percentages-configurator'>
+        {renderMessages()}
         <h1>Configuración de porcentajes</h1>
         <p className='description'>Configura los porcentajes de cada examen para el cálculo de la nota final.</p>
         <div className='percentages-configurator'>
-          <PercentagesForm formValues={formValues} handleChange={handleChange} />
+          <PercentagesForm
+            formValues={formValues}
+            handleChange={handleChange}
+            onSave={handleSave}
+            loading={loading}
+          />
         </div>
       </div>
     </SectionLayout>
