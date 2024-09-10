689e88e128aa47bd787bff620cad6499bc89101d
Charly <carlosgarita3110@gmail.com>
2024-09-09 22:17:03 -0600

    feat: integrate API for fetching questions and handle loading state
    - Added handleGetAllQuestions to fetch questions from API on component mount.
    - Included loading state to manage and display loading status.
    - Passed loading state to QuestionList component.
    - Updated handleDelete to remove questions from local state and API.

--- management_client/src/components/modules/questions/delete_questions/view/DeleteQuestionView.jsx
+++ management_client/src/components/modules/questions/delete_questions/view/DeleteQuestionView.jsx
@@ -1,8 +1,7 @@
 import { useState, useEffect } from 'react'
 import QuestionList from '../organisms/QuestionList'
 import '../../../../../assets/styles/global/view.css'
-import { dummyData } from '../../helpers/dummyData'
-import { handleDeleteFromList } from '../helpers/formHandlers'
+import { handleDeleteFromList, handleGetAllQuestions } from '../helpers/formHandlers'
 import FindQuestion from '../../base/molecules/FindQuestion'
 import SectionLayout from '../../../../core/global/molecules/SectionLayout'
 import useMessages from '../../../../core/global/hooks/useMessages'
@@ -11,11 +10,11 @@ const DeleteQuestionView = () => {
   const [questions, setQuestions] = useState([])
   const [filteredQuestions, setFilteredQuestions] = useState([])
   const { setErrorMessage, setSuccessMessage, renderMessages } = useMessages()
+  const [loading, setLoading] = useState(false)
 
-  useEffect(() => {
-    setQuestions(dummyData)
-    setFilteredQuestions(dummyData)
-  }, [])
+  useEffect(() =>
+    handleGetAllQuestions(setQuestions, setLoading, setErrorMessage)
+  , [])
 
   const handleDelete = (code) => {
     handleDeleteFromList(code, filteredQuestions, setFilteredQuestions, setErrorMessage, setSuccessMessage)
@@ -31,6 +30,7 @@ const DeleteQuestionView = () => {
       <QuestionList
         questions={filteredQuestions}
         onDelete={handleDelete}
+        loading={loading}
       />
       {renderMessages()}
     </SectionLayout>
