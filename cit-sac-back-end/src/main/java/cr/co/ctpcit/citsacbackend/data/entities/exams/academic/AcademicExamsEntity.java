300d0485fc2ea6071a75e766766616b523678b2a
Alessandro Cambronero Delgado <134018906+MSIALE2023@users.noreply.github.com>
2024-09-14 11:16:42 -0600

    feat: add entity AcademicExamsEntity

--- cit-sac-back-end/src/main/java/cr/co/ctpcit/citsacbackend/data/entities/exams/academic/AcademicExamsEntity.java
+++ cit-sac-back-end/src/main/java/cr/co/ctpcit/citsacbackend/data/entities/exams/academic/AcademicExamsEntity.java
@@ -1,5 +1,39 @@
 package cr.co.ctpcit.citsacbackend.data.entities.exams.academic;
 
+import cr.co.ctpcit.citsacbackend.data.entities.inscription.EnrollmentEntity;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import lombok.*;
 
+import java.math.BigDecimal;
+import java.util.Date;
+
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@ToString
+@Entity
+@Table(name = "tbl_academicexams")
 public class AcademicExamsEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "exam_id", nullable = false)
+    private Integer id;
+
+    @NotNull
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "enrollment_id", nullable = false)
+    @ToString.Exclude
+    private EnrollmentEntity enrollment;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    @Column(name = "exam_date", nullable = false)
+    private Date examDate;
+
+    @NotNull
+    @Column(name = "grade", precision = 5, scale = 2, nullable = false)
+    private BigDecimal grade;
 }
