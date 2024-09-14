b08d29392811227edcadaeb6b745624be64d2f49
Alessandro Cambronero Delgado <134018906+MSIALE2023@users.noreply.github.com>
2024-09-14 11:08:27 -0600

    ref: redefine code for the DAI Exams module

---
+++ cit-sac-back-end/src/main/java/cr/co/ctpcit/citsacbackend/data/repositories/DaiExamQuestionsRepository.java
@@ -0,0 +1,14 @@
+package cr.co.ctpcit.citsacbackend.data.repositories;
+
+import cr.co.ctpcit.citsacbackend.data.entities.exams.dai.DaiExamQuestionsEntity;
+import cr.co.ctpcit.citsacbackend.data.entities.exams.dai.DaiExamQuestionsEntityId;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface DaiExamQuestionsRepository extends JpaRepository<DaiExamQuestionsEntity, DaiExamQuestionsEntityId> {
+    @Query("SELECT CASE WHEN COUNT(a) > 0 THEN true ELSE false END FROM DaiExamQuestionsEntity a WHERE a.id.questionId = :questionId")
+    boolean existsByQuestionId(@Param("questionId") Integer questionId);
+}
---
+++ cit-sac-back-end/src/main/java/cr/co/ctpcit/citsacbackend/logic/services/examsImplementations/DaiQuestionsServiceImplementation.java
@@ -0,0 +1,63 @@
+package cr.co.ctpcit.citsacbackend.logic.services.examsImplementations;
+
+import cr.co.ctpcit.citsacbackend.data.entities.exams.dai.DaiQuestionsEntity;
+import cr.co.ctpcit.citsacbackend.data.repositories.DaiExamQuestionsRepository;
+import cr.co.ctpcit.citsacbackend.data.repositories.DaiQuestionsRepository;
+import cr.co.ctpcit.citsacbackend.logic.dto.exams.dai.DaiQuestionsDto;
+import cr.co.ctpcit.citsacbackend.logic.mappers.exams.dai.DaiQuestionsMapper;
+import cr.co.ctpcit.citsacbackend.logic.services.DaiQuestionsService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.NoSuchElementException;
+
+@Service
+public class DaiQuestionsServiceImplementation implements DaiQuestionsService {
+
+    @Autowired
+    private DaiQuestionsRepository daiQuestionsRepository;
+
+    @Autowired
+    private DaiExamQuestionsRepository daiExamQuestionsRepository;
+
+    @Autowired
+    public DaiQuestionsServiceImplementation(DaiQuestionsRepository repository) {
+        this.daiQuestionsRepository = repository;
+    }
+
+    @Override
+    public List<DaiQuestionsDto> obtenerTodasLasPreguntas() {
+        List<DaiQuestionsEntity> entities = daiQuestionsRepository.findAll();
+        return DaiQuestionsMapper.toDtoList(entities);
+    }
+
+    @Override
+    public DaiQuestionsDto obtenerPreguntaPorId(Integer id) {
+        DaiQuestionsEntity entity = daiQuestionsRepository.findById(id)
+                .orElseThrow(() -> new NoSuchElementException("Pregunta no encontrada con el id " + id));
+        return DaiQuestionsMapper.toDto(entity);
+    }
+
+    @Override
+    public void eliminarPregunta(Integer questionId) {
+        if (daiExamQuestionsRepository.existsByQuestionId(questionId)) {
+            throw new IllegalStateException("No se puede eliminar la pregunta porque está asociada a un examen.");
+        }
+        daiQuestionsRepository.deleteById(questionId);
+    }
+
+    @Override
+    public DaiQuestionsDto modificarPregunta(Integer id, DaiQuestionsDto preguntaDto) {
+        DaiQuestionsEntity entity = daiQuestionsRepository.findById(id)
+                .orElseThrow(() -> new NoSuchElementException("Pregunta no encontrada con el id " + id));
+
+        entity.setQuestionText(preguntaDto.questionText());
+        entity.setQuestionGrade(preguntaDto.questionGrade());
+        entity.setImageUrl(preguntaDto.imageUrl());
+
+        DaiQuestionsEntity updatedEntity = daiQuestionsRepository.save(entity);
+        return DaiQuestionsMapper.toDto(updatedEntity);
+    }
+
+}
