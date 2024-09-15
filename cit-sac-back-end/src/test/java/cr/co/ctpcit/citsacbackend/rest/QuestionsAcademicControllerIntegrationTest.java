58e39920daf542510483e1aa812d55c86e80decf
Alessandro Cambronero Delgado <134018906+MSIALE2023@users.noreply.github.com>
2024-09-14 20:01:09 -0600

    test: Integration Tests for Exam Module

---
+++ cit-sac-back-end/src/test/java/cr/co/ctpcit/citsacbackend/rest/QuestionsAcademicControllerIntegrationTest.java
@@ -0,0 +1,65 @@
+package cr.co.ctpcit.citsacbackend.rest;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import cr.co.ctpcit.citsacbackend.logic.dto.exams.academic.AcademicQuestionsDto;
+import org.junit.jupiter.api.Test;
+import cr.co.ctpcit.citsacbackend.data.enums.Grades;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@Transactional
+@Rollback
+public class QuestionsAcademicControllerIntegrationTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @Test
+    public void testGetAllExamQuestions() throws Exception {
+        mockMvc.perform(get("/api/Academic"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$").isArray());
+    }
+
+    @Test
+    public void testGetExamQuestionById() throws Exception {
+        int testId = 1;
+        mockMvc.perform(get("/api/Academic/{id}", testId))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$.id").value(testId));
+    }
+
+    @Test
+    public void testDeleteExamQuestion() throws Exception {
+        int testId = 1;
+        mockMvc.perform(delete("/api/Academic/{id}", testId))
+                .andExpect(status().isNoContent());
+    }
+
+    @Test
+    public void testUpdateExamQuestion() throws Exception {
+        int testId = 1;
+        AcademicQuestionsDto preguntaDto = new AcademicQuestionsDto(testId, "Al resolver la ecuación: 2x+1=5, obtenemos", Grades.SEVENTH, "3", "2", "1", "5", "B", null);
+
+        mockMvc.perform(put("/api/Academic/{id}", testId)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(preguntaDto)))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.questionText").value("Al resolver la ecuación: 2x+1=5, obtenemos"));
+    }
+}
