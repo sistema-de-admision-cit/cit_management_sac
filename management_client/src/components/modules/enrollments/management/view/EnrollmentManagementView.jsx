86dfc58d591fe894e02470860c8ec2faa7160f5a
Charly <carlosgarita3110@gmail.com>
2024-09-15 19:58:46 -0600

    feat(enrollment): add search functionality to EnrollmentManagementView with EnrollmentSearchBar component and update event handlers

--- management_client/src/components/modules/enrollments/management/view/EnrollmentManagementView.jsx
+++ management_client/src/components/modules/enrollments/management/view/EnrollmentManagementView.jsx
@@ -3,11 +3,12 @@ import SectionLayout from '../../../../core/global/molecules/SectionLayout'
 import EnrollmentTable from '../organisms/EnrollmentTable'
 import '../../../../../assets/styles/enrollments/enrollment-management-view.css'
 import { dummyData } from './temp_data'
+import EnrollemntSearchBar from '../molecules/EnrollmentSearchBar'
 
 const EnrollmentManagementView = ({ enrollments }) => {
   const [applicants, setApplicants] = useState(dummyData)
 
-  const handleCedulaClick = (aspirante) => {
+  const handleStudendIdClick = (aspirante) => {
     console.log('Cédula:', aspirante.cedula)
   }
 
@@ -23,14 +24,27 @@ const EnrollmentManagementView = ({ enrollments }) => {
     console.log('Documento:', type)
   }
 
+  const handleSearch = (search) => {
+    const filteredApplicants = dummyData.filter((applicant) => {
+      return applicant.studendtId.includes(search) ||
+        applicant.firstName.toLowerCase().includes(search.toLowerCase()) ||
+        applicant.firstSurname.includes(search.toLowerCase()) ||
+        applicant.secondSurname.toLowerCase().includes(search.toLowerCase())
+    })
+
+    setApplicants(filteredApplicants)
+  }
+
   return (
     <SectionLayout title='Consultar Inscripciones'>
       <div className='enrollment-management-view'>
         <h1>Consultar Inscripciones</h1>
         <p className='description'>Aquí puedes consultar y gestionar las inscripciones de los aspirantes.</p>
+
+        <EnrollemntSearchBar onSearch={handleSearch} />
         <EnrollmentTable
           applicants={applicants}
-          onCedulaClick={handleCedulaClick}
+          onStudentIdClick={handleStudendIdClick}
           onDateChange={handleDateChange}
           onWhatsappChange={handleWhatsappChange}
           onDocClick={handleDocClick}
