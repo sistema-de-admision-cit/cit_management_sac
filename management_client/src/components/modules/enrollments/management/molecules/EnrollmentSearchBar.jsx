648e33596ddb07724b515db0b023612fc45fbdfa
Charly <carlosgarita3110@gmail.com>
2024-09-15 20:01:10 -0600

    feat(enrollment): add EnrollmentSearchBar component for searching inscriptions

---
+++ management_client/src/components/modules/enrollments/management/molecules/EnrollmentSearchBar.jsx
@@ -0,0 +1,32 @@
+import { useState } from 'react'
+import '../../../../../assets/styles/enrollments/enrollment-search-bar.css'
+import InputField from '../../../../core/global/atoms/InputField'
+
+const EnrollemntSearchBar = ({ onSearch }) => {
+  const [search, setSearch] = useState('')
+
+  const handleSearch = (e) => {
+    setSearch(e.target.value)
+    onSearch(e.target.value)
+  }
+
+  return (
+    <div className='search-bar-container'>
+      <div className='enrollment-search-bar'>
+        <InputField
+          field={{
+            name: 'search',
+            label: 'Buscar Inscripción',
+            type: 'text',
+            placeholder: 'Buscar por cedula, nombre o apellidos'
+          }}
+          value={search}
+          handleChange={handleSearch}
+          className='form-group'
+        />
+      </div>
+    </div>
+  )
+}
+
+export default EnrollemntSearchBar
