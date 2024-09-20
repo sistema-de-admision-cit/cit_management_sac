ac3f1ad5bbd502f6324902f137c473138f63d621
Charly <carlosgarita3110@gmail.com>
2024-09-20 17:34:47 -0600

    refactor: simplify file management logic in ModalManageFiles component

--- management_client/src/components/modules/enrollments/management/molecules/ModalManageFiles.jsx
+++ management_client/src/components/modules/enrollments/management/molecules/ModalManageFiles.jsx
@@ -3,38 +3,40 @@ import InputField from '../../../../core/global/atoms/InputField'
 import Modal from '../../../../core/global/molecules/Modal'
 import '../../../../../assets/styles/enrollments/modal-manage-files.css'
 
-const ModalManageFiles = ({ selectedColumn, selectedFiles, onFileUpload, onFileDownload, onFileDelete, onClose }) => (
+const ModalManageFiles = ({ selectedColumn, selectedFile, onFileUpload, onFileDownload, onFileDelete, onClose }) => (
   <Modal onClose={onClose}>
     <h2>Gestionar {selectedColumn}</h2>
-    {selectedFiles.length === 0 && (
-      <InputField
-        field={{ type: 'file', name: 'file', id: 'file', label: 'Subir archivo' }}
-        onChange={onFileUpload}
-        className='form-group'
-      />
-    )}
+    {!selectedFile
+      ? (
+        <InputField
+          field={{ type: 'file', name: 'file', id: 'file', label: 'Subir archivo' }}
+          onChange={onFileUpload}
+          className='form-group'
+        />
+        )
+      : (
+        <div className='file-list-container'>
+          <ul className='file-list'>
 
-    <div className='file-list-container'>
-      <ul className='file-list'>
-        {selectedFiles.map((file, index) => (
-          <li key={index} className='file-list-item'>
-            {file.name}
-            <Button
-              onClick={() => onFileDownload(file)}
-              className='btn btn-primary'
-            >
-              Descargar
-            </Button>
-            <Button
-              onClick={() => onFileDelete(file)}
-              className='btn btn-danger'
-            >
-              Eliminar
-            </Button>
-          </li>
-        ))}
-      </ul>
-    </div>
+            <li className='file-list-item'>
+              {selectedFile.documentName}
+              <Button
+                onClick={() => onFileDownload(selectedFile)}
+                className='btn btn-primary'
+              >
+                Descargar
+              </Button>
+              <Button
+                onClick={() => onFileDelete(selectedFile)}
+                className='btn btn-danger'
+              >
+                Eliminar
+              </Button>
+            </li>
+          </ul>
+        </div>
+        )}
+
   </Modal>
 )
 
