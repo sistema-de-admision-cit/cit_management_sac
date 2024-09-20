50ea9197a0824081a4154bb87c23793839325837
Charly <carlosgarita3110@gmail.com>
2024-09-20 09:23:51 -0600

    refactor(button): add key prop to Button component

--- management_client/src/components/core/global/atoms/Button.jsx
+++ management_client/src/components/core/global/atoms/Button.jsx
@@ -1,8 +1,8 @@
 import '../../../../assets/styles/global/buttons.css'
 
-const Button = ({ type, className, children, onClick, disabled }) => {
+const Button = ({ type, className, children, onClick, disabled, key }) => {
   return (
-    <button type={type} className={className} onClick={onClick} disabled={disabled || false}>
+    <button key={key} type={type} className={className} onClick={onClick} disabled={disabled || false}>
       {children}
     </button>
   )
