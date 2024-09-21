d47b8cad099b526615c596c10ad65846522c7c9a
Charly <carlosgarita3110@gmail.com>
2024-09-21 16:29:24 -0600

    refactor(styles): update modal content class name handling

--- management_client/src/components/core/global/molecules/Modal.jsx
+++ management_client/src/components/core/global/molecules/Modal.jsx
@@ -2,7 +2,7 @@ import '../../../../assets/styles/global/modal.css'
 
 const Modal = ({ children, onClose, className }) => (
   <div className='modal'>
-    <div className={`modal-content ${className}`}>
+    <div className={`modal-content ${className || ''}`}>
       <span className='close-btn' onClick={onClose} aria-label='Close'>&times;</span>
       {children}
     </div>
