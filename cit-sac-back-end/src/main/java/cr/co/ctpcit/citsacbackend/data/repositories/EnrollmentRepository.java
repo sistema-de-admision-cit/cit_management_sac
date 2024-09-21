ce1ddd8fafccd6721d2cfc5fc3b6f9c3355607b6
Charly <carlosgarita3110@gmail.com>
2024-09-21 10:46:02 -0600

    feat: add update query for whatsapp notification in EnrollmentRepository

--- cit-sac-back-end/src/main/java/cr/co/ctpcit/citsacbackend/data/repositories/EnrollmentRepository.java
+++ cit-sac-back-end/src/main/java/cr/co/ctpcit/citsacbackend/data/repositories/EnrollmentRepository.java
@@ -1,7 +1,15 @@
 package cr.co.ctpcit.citsacbackend.data.repositories;
 
 import cr.co.ctpcit.citsacbackend.data.entities.inscription.EnrollmentEntity;
+import jakarta.transaction.Transactional;
+import jakarta.validation.constraints.NotNull;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 
 public interface EnrollmentRepository extends JpaRepository<EnrollmentEntity, Long> {
+    @Modifying
+    @Transactional
+    @Query("UPDATE EnrollmentEntity e SET e.whatsappNotification = :whatsappNotification WHERE e.id = :id")
+    void updateWhatsappNotificationById(@NotNull Long id, @NotNull Boolean whatsappNotification);
 }
