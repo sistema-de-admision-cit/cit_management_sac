fb43e0aeb1ca5f8f96800f53097720932167bcd4
Charly <carlosgarita3110@gmail.com>
2024-09-21 10:27:49 -0600

    fix: update student ID field name in ModalApplicantDetails

--- management_client/src/components/modules/enrollments/management/molecules/ModalApplicantDetails.jsx
+++ management_client/src/components/modules/enrollments/management/molecules/ModalApplicantDetails.jsx
@@ -14,7 +14,7 @@ const ModalApplicantDetails = ({ student, parentsGuardians, onClose }) => {
       <p><strong>Nombre:</strong> {student.firstName} {student.firstSurname} {student.secondSurname}</p>
       <p><strong>Fecha de Nacimiento:</strong> {student.birthDate}</p>
       <p><strong>Tipo de ID:</strong> {student.idType}</p>
-      <p><strong>Número de ID:</strong> {student.studendtId}</p>
+      <p><strong>Número de ID:</strong> {student.idNumber}</p>
       <p><strong>Escuela Anterior:</strong> {student.previousSchool}</p>
       <p><strong>Adaptaciones:</strong> {student.hasAccommodations ? 'Sí' : 'No'}</p>
     </div>
