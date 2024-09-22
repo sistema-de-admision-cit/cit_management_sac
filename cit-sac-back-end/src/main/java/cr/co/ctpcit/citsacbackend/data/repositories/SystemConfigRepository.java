44da5622a5777ffa3a9def66a068bf8afa204bd9
Charly <carlosgarita3110@gmail.com>
2024-09-22 15:20:00 -0600

    feat(repository): add method to search by config name
    - Added method `getSystemConfigEntitiesByConfigNameContaining` to filter SystemConfigEntities based on config name.
    - Annotated the interface with @Repository for clarity.

--- cit-sac-back-end/src/main/java/cr/co/ctpcit/citsacbackend/data/repositories/SystemConfigRepository.java
+++ cit-sac-back-end/src/main/java/cr/co/ctpcit/citsacbackend/data/repositories/SystemConfigRepository.java
@@ -2,6 +2,11 @@ package cr.co.ctpcit.citsacbackend.data.repositories;
 
 import cr.co.ctpcit.citsacbackend.data.entities.SystemConfigEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
+@Repository
 public interface SystemConfigRepository extends JpaRepository<SystemConfigEntity, Integer> {
+  List<SystemConfigEntity> getSystemConfigEntitiesByConfigNameContaining(String configName);
 }
