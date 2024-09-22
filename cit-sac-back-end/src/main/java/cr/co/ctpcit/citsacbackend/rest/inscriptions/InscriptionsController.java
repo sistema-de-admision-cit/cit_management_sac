2b4d17dfb53b69310b95007e99be0755f35919b0
Charly <carlosgarita3110@gmail.com>
2024-09-22 10:31:09 -0600

    feat: add endpoint to delete documents
    - Implemented DELETE /documents/delete/{id} endpoint
    - Added functionality to delete a document from the database and storage
    - Returns 200 OK if deletion is successful, 404 Not Found otherwise
    - Updated method documentation for clarity

--- cit-sac-back-end/src/main/java/cr/co/ctpcit/citsacbackend/rest/inscriptions/InscriptionsController.java
+++ cit-sac-back-end/src/main/java/cr/co/ctpcit/citsacbackend/rest/inscriptions/InscriptionsController.java
@@ -1,12 +1,13 @@
 package cr.co.ctpcit.citsacbackend.rest.inscriptions;
 
-import cr.co.ctpcit.citsacbackend.data.entities.inscription.EnrollmentEntity;
 import cr.co.ctpcit.citsacbackend.data.enums.ProcessStatus;
 import cr.co.ctpcit.citsacbackend.logic.dto.inscription.StudentDto;
 import cr.co.ctpcit.citsacbackend.logic.services.inscriptions.InscriptionsService;
 import cr.co.ctpcit.citsacbackend.logic.services.storage.StorageService;
 import jakarta.validation.ConstraintViolationException;
-import jakarta.validation.constraints.*;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.Resource;
 import org.springframework.data.domain.Pageable;
@@ -23,135 +24,153 @@ import java.util.List;
 @RequestMapping("/api/inscriptions")
 @Validated
 public class InscriptionsController {
-    private final InscriptionsService inscriptionsService;
-    private final StorageService storageService;
+  private final InscriptionsService inscriptionsService;
+  private final StorageService storageService;
 
-    @Autowired
-    public InscriptionsController(InscriptionsService inscriptionsService,
-            StorageService storageService) {
-        this.inscriptionsService = inscriptionsService;
-        this.storageService = storageService;
-    }
+  @Autowired
+  public InscriptionsController(InscriptionsService inscriptionsService,
+      StorageService storageService) {
+    this.inscriptionsService = inscriptionsService;
+    this.storageService = storageService;
+  }
 
-    /**
-     * Get an inscription by id
-     *
-     * @param id the id of the student
-     * @return the inscription with the given id
-     */
-    @GetMapping("/{id}")
-    public ResponseEntity<StudentDto> getInscriptionById(@PathVariable("id") Long id) {
-        StudentDto student = inscriptionsService.findStudentById(id);
-        return student == null ? ResponseEntity.notFound().build() : ResponseEntity.ok(student);
-    }
+  /**
+   * Get an inscription by id
+   *
+   * @param id the id of the student
+   * @return the inscription with the given id
+   */
+  @GetMapping("/{id}")
+  public ResponseEntity<StudentDto> getInscriptionById(@PathVariable("id") Long id) {
+    StudentDto student = inscriptionsService.findStudentById(id);
+    return student == null ? ResponseEntity.notFound().build() : ResponseEntity.ok(student);
+  }
 
-    /**
-     * Get an inscription by value
-     *
-     * @param value the name of the student
-     * @return the inscription with the given name
-     */
-    @GetMapping("/search")
-    public ResponseEntity<Iterable<StudentDto>> getInscriptionsByValue(
-            @NotNull @RequestParam String value) {
-        List<StudentDto> student = inscriptionsService.findStudentByValue(value);
-        return student == null ? ResponseEntity.notFound().build() : ResponseEntity.ok(student);
-    }
+  /**
+   * Get an inscription by value
+   *
+   * @param value the name of the student
+   * @return the inscription with the given name
+   */
+  @GetMapping("/search")
+  public ResponseEntity<Iterable<StudentDto>> getInscriptionsByValue(
+      @NotNull @RequestParam String value) {
+    List<StudentDto> student = inscriptionsService.findStudentByValue(value);
+    return student == null ? ResponseEntity.notFound().build() : ResponseEntity.ok(student);
+  }
 
-    /**
-     * Get all inscriptions
-     *
-     * @return a list of all inscriptions
-     */
-    @GetMapping
-    public ResponseEntity<Iterable<StudentDto>> getInscriptions(
-            @PageableDefault(page = 0, size = 25) Pageable pageable) {
-        List<StudentDto> inscriptions = inscriptionsService.getAllInscriptions(pageable);
-        return inscriptions.isEmpty() ? ResponseEntity.noContent().build()
-                : ResponseEntity.ok(inscriptions);
-    }
+  /**
+   * Get all inscriptions
+   *
+   * @return a list of all inscriptions
+   */
+  @GetMapping
+  public ResponseEntity<Iterable<StudentDto>> getInscriptions(
+      @PageableDefault(page = 0, size = 25) Pageable pageable) {
+    List<StudentDto> inscriptions = inscriptionsService.getAllInscriptions(pageable);
+    return inscriptions.isEmpty() ?
+        ResponseEntity.noContent().build() :
+        ResponseEntity.ok(inscriptions);
+  }
 
-    /**
-     * Update exam date
-     *
-     * @param id the id of the enrollment
-     * @param date the new date of the exam
-     * @return the updated enrollment
-     */
-    @PutMapping("/{id}/exam")
-    public ResponseEntity<StudentDto> updateExamDate(@PathVariable("id") String id,
-            @NotNull @NotEmpty @Pattern(regexp = "^\\d{4}-\\d{2}-\\d{2}$",
-                    message = "Formato de fecha incorrecto. Use yyyy-MM-dd") @RequestParam String date) {
-        StudentDto student = inscriptionsService.updateExamDate(id, date);
-        return student == null ? ResponseEntity.notFound().build() : ResponseEntity.ok(student);
-    }
+  /**
+   * Update exam date
+   *
+   * @param id   the id of the enrollment
+   * @param date the new date of the exam
+   * @return the updated enrollment
+   */
+  @PutMapping("/{id}/exam")
+  public ResponseEntity<StudentDto> updateExamDate(@PathVariable("id") String id, @NotNull @NotEmpty
+  @Pattern(regexp = "^\\d{4}-\\d{2}-\\d{2}$",
+      message = "Formato de fecha incorrecto. Use yyyy-MM-dd") @RequestParam String date) {
+    StudentDto student = inscriptionsService.updateExamDate(id, date);
+    return student == null ? ResponseEntity.notFound().build() : ResponseEntity.ok(student);
+  }
 
-    // TODO: Create update endpoint to alter status
+  // TODO: Create update endpoint to alter status
 
-    /**
-     * @param id the id of the enrollment
-     * @param status the new status of the enrollment
-     * @return ok if the status was updated, not found otherwise
-     */
-    @PutMapping("/update-status/{id}")
-    public ResponseEntity<StudentDto> updateStatus(@PathVariable("id") Long id,
-            @RequestParam @NotNull ProcessStatus status) {
-        boolean updated = inscriptionsService.changeStatus(id, status);
-        return updated ? ResponseEntity.ok().build() : ResponseEntity.notFound().build();
-    }
+  /**
+   * @param id     the id of the enrollment
+   * @param status the new status of the enrollment
+   * @return ok if the status was updated, not found otherwise
+   */
+  @PutMapping("/update-status/{id}")
+  public ResponseEntity<StudentDto> updateStatus(@PathVariable("id") Long id,
+      @RequestParam @NotNull ProcessStatus status) {
+    boolean updated = inscriptionsService.changeStatus(id, status);
+    return updated ? ResponseEntity.ok().build() : ResponseEntity.notFound().build();
+  }
 
-    /**
-     * Change whatsapp notification permission
-     *
-     * @param id the id of the student
-     * @param permission the new permission
-     * @return ok if the permission was updated, not found otherwise
-     */
-    @PutMapping("/update-whatsapp/{id}")
-    public ResponseEntity<StudentDto> changeWhatsappPermission(@PathVariable("id") Long id,
-            @RequestParam @NotNull Boolean permission) {
-        boolean updated = inscriptionsService.changeWhatsappPermission(id, permission);
-        return updated ? ResponseEntity.ok().build() : ResponseEntity.notFound().build();
-    }
+  /**
+   * Change whatsapp notification permission
+   *
+   * @param id         the id of the student
+   * @param permission the new permission
+   * @return ok if the permission was updated, not found otherwise
+   */
+  @PutMapping("/update-whatsapp/{id}")
+  public ResponseEntity<StudentDto> changeWhatsappPermission(@PathVariable("id") Long id,
+      @RequestParam @NotNull Boolean permission) {
+    boolean updated = inscriptionsService.changeWhatsappPermission(id, permission);
+    return updated ? ResponseEntity.ok().build() : ResponseEntity.notFound().build();
+  }
 
-    @PutMapping("/update-enrollment/{enrollmentId}")
-    public ResponseEntity<String> updateEnrollment(@PathVariable("enrollmentId") Long enrollmentId,
-            @RequestParam @NotNull ProcessStatus status,
-            @RequestParam @NotNull String examDate,
-            @RequestParam @NotNull String whatsappPermission,
-            @RequestParam String comment,
-            @RequestParam Integer changedBy
-    ) {
-        Boolean updated = inscriptionsService.updateEnrollment(enrollmentId, status, examDate,
-                whatsappPermission, comment, changedBy);
+  @PutMapping("/update-enrollment/{enrollmentId}")
+  public ResponseEntity<String> updateEnrollment(@PathVariable("enrollmentId") Long enrollmentId,
+      @RequestParam @NotNull ProcessStatus status, @RequestParam @NotNull String examDate,
+      @RequestParam @NotNull String whatsappPermission, @RequestParam String comment,
+      @RequestParam Integer changedBy) {
+    Boolean updated =
+        inscriptionsService.updateEnrollment(enrollmentId, status, examDate, whatsappPermission,
+            comment, changedBy);
 
-        return updated ? ResponseEntity.noContent().build() : ResponseEntity.notFound().build();
-    }
+    return updated ? ResponseEntity.noContent().build() : ResponseEntity.notFound().build();
+  }
 
-    // TODO: Create update endpoint to alter documents
+  // TODO: Create update endpoint to alter documents
 
-    /**
-     * Download documents
-     *
-     * @param filename the name of the document
-     * @return the document as a resource
-     */
-    @GetMapping("/documents/{filename}")
-    public ResponseEntity<Resource> downloadDocuments(@PathVariable String filename) {
-        Resource resource = storageService.loadAsResource(filename);
-        return ResponseEntity.ok().header(HttpHeaders.CONTENT_DISPOSITION,
-                "attachment; filename=\"" + resource.getFilename() + "\"").body(resource);
-    }
+  /**
+   * Download documents
+   *
+   * @param filename the name of the document
+   * @return the document as a resource
+   */
+  @GetMapping("/documents/download/{filename}")
+  public ResponseEntity<Resource> downloadDocuments(@PathVariable String filename) {
+    Resource resource = storageService.loadAsResource(filename);
+    return ResponseEntity.ok().header(HttpHeaders.CONTENT_DISPOSITION,
+        "attachment; filename=\"" + resource.getFilename() + "\"").body(resource);
+  }
 
-    /**
-     * Handle constraint violation exceptions
-     *
-     * @param e the exception
-     * @return a response entity with the error message
-     */
-    @ExceptionHandler(ConstraintViolationException.class)
-    @ResponseStatus(HttpStatus.BAD_REQUEST)
-    ResponseEntity<String> handleConstraintViolationException(ConstraintViolationException e) {
-        return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
-    }
+  /**
+   * Delete a document
+   *
+   * @param id       the id of the document
+   * @param filename the path of the document in the static/files document folder
+   * @return ok if the document was deleted, not found otherwise
+   */
+  @DeleteMapping("/documents/delete/{id}")
+  public ResponseEntity<String> deleteDocument(@PathVariable Long id,
+      @RequestParam String filename) {
+    boolean databaseDeleted = inscriptionsService.deleteDocument(id);
+
+    boolean storageDeleted = storageService.deleteDocument(filename);
+
+    return databaseDeleted && storageDeleted ?
+        ResponseEntity.ok().build() :
+        ResponseEntity.notFound().build();
+  }
+
+  /**
+   * Handle constraint violation exceptions
+   *
+   * @param e the exception
+   * @return a response entity with the error message
+   */
+  @ExceptionHandler(ConstraintViolationException.class)
+  @ResponseStatus(HttpStatus.BAD_REQUEST)
+  ResponseEntity<String> handleConstraintViolationException(ConstraintViolationException e) {
+    return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
+  }
 }
