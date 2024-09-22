56becb95e4061832831c948e52a04f0c13271046
Charly <carlosgarita3110@gmail.com>
2024-09-21 22:09:40 -0600

    feat: add stored procedure for enrollment updates and logging
    - Introduced `usp_update_enrollment_and_log` procedure for batch updates.
    - Enhanced repository with additional parameters for exam date and change details.
    - Improved method structure for clearer parameter naming.

--- cit-sac-back-end/src/main/java/cr/co/ctpcit/citsacbackend/data/repositories/EnrollmentRepository.java
+++ cit-sac-back-end/src/main/java/cr/co/ctpcit/citsacbackend/data/repositories/EnrollmentRepository.java
@@ -7,6 +7,10 @@ import jakarta.validation.constraints.NotNull;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.jpa.repository.query.Procedure;
+import org.springframework.data.repository.query.Param;
+
+import java.sql.Date;
 
 public interface EnrollmentRepository extends JpaRepository<EnrollmentEntity, Long> {
     @Modifying
@@ -18,4 +22,14 @@ public interface EnrollmentRepository extends JpaRepository<EnrollmentEntity, Lo
     @Transactional
     @Query("UPDATE EnrollmentEntity e SET e.status = :status WHERE e.id = :id")
     void updateStatusById(Long id, ProcessStatus status);
+
+    @Procedure(name = "usp_update_enrollment_and_log")
+    void usp_update_enrollment_and_log(
+            @Param("p_enrollment_id") Long enrollmentId,
+            @Param("p_new_status") String newStatus,
+            @Param("p_new_exam_date") Date newExamDate,
+            @Param("p_new_whatsapp_permission") Boolean newWhatsappPermission,
+            @Param("p_comment") String comment,
+            @Param("p_changed_by") Integer changedBy
+    );
 }
