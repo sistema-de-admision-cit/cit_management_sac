9cd03ad6ef2a4fa58c55cb2cdbf1c55dcdc42fbb
Charly <carlosgarita3110@gmail.com>
2024-09-23 17:04:35 -0600

    feat: add EnglishExamService dependency to EnglishExamController

--- cit-sac-back-end/src/main/java/cr/co/ctpcit/citsacbackend/rest/exams/english/EnglishExamController.java
+++ cit-sac-back-end/src/main/java/cr/co/ctpcit/citsacbackend/rest/exams/english/EnglishExamController.java
@@ -1,5 +1,6 @@
 package cr.co.ctpcit.citsacbackend.rest.exams.english;
 
+import cr.co.ctpcit.citsacbackend.logic.services.exams.english.EnglishExamService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,6 +8,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("api/exams/english")
 public class EnglishExamController {
+  private final EnglishExamService englishExamService;
+
+  public EnglishExamController(EnglishExamService englishExamService) {
+    this.englishExamService = englishExamService;
+  }
+
   @GetMapping("/health")
   public String getEnglishExams() {
     return "English exams are healthy!";
