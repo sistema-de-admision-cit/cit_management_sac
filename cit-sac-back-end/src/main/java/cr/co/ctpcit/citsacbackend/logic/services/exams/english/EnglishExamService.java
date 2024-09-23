332249842a9a352278d7ea6dc84724aa5e3d4b26
Charly <carlosgarita3110@gmail.com>
2024-09-23 14:42:34 -0600

    feat: add basic CRUD operations to EnglishExamService
    - Implemented getAll() method to retrieve all English exam entities.
    - Added getById(Long id) method to fetch a specific English exam entity by ID.
    - Introduced save(EnglishExamEntity englishExamEntity) method for saving an English exam entity.
    - Created delete(Long id) method to remove an English exam entity by ID.

--- cit-sac-back-end/src/main/java/cr/co/ctpcit/citsacbackend/logic/services/exams/english/EnglishExamService.java
+++ cit-sac-back-end/src/main/java/cr/co/ctpcit/citsacbackend/logic/services/exams/english/EnglishExamService.java
@@ -1,7 +1,10 @@
 package cr.co.ctpcit.citsacbackend.logic.services.exams.english;
 
+import cr.co.ctpcit.citsacbackend.data.entities.exams.english.EnglishExamEntity;
 import cr.co.ctpcit.citsacbackend.data.repositories.exam.english.EnglishExamRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class EnglishExamService {
@@ -9,5 +12,21 @@ public class EnglishExamService {
 
   public EnglishExamService(EnglishExamRepository englishExamRepository) {
     this.englishExamRepository = englishExamRepository;
+  }
+
+  public List<EnglishExamEntity> getAll() {
+    return englishExamRepository.findAll();
+  }
+
+  public EnglishExamEntity getById(Long id) {
+    return englishExamRepository.findById(id).orElse(null);
+  }
+
+  public EnglishExamEntity save(EnglishExamEntity englishExamEntity) {
+    return englishExamRepository.save(englishExamEntity);
+  }
+
+  public void delete(Long id) {
+    englishExamRepository.deleteById(id);
   }
 }
