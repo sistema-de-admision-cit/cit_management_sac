433db5db452c7469fead19ab19b3a16e94e064fe
Charly <carlosgarita3110@gmail.com>
2024-09-23 17:04:12 -0600

    feat: add health check endpoint to EnglishExamController

--- cit-sac-back-end/src/main/java/cr/co/ctpcit/citsacbackend/rest/exams/english/EnglishExamController.java
+++ cit-sac-back-end/src/main/java/cr/co/ctpcit/citsacbackend/rest/exams/english/EnglishExamController.java
@@ -1,10 +1,15 @@
 package cr.co.ctpcit.citsacbackend.rest.exams.english;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("api/exams/english")
 public class EnglishExamController {
+  @GetMapping("/health")
+  public String getEnglishExams() {
+    return "English exams are healthy!";
+  }
 
 }
