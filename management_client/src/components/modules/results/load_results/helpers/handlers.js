bccb94f850faf31a0b29f578f75171609a5b82c0
Charly <carlosgarita3110@gmail.com>
2024-09-23 13:49:25 -0600

    feat: add fetchTrackTestScores function signature for fetching track test scores from API

---
+++ management_client/src/components/modules/results/load_results/helpers/handlers.js
@@ -0,0 +1,7 @@
+export const fetchTrackTestScores = async (setSuccessMessage, setErrorMessage) => {
+  // todo: fetch track test scores from the API
+  // axios.get('/api/track-test-scores')
+  setTimeout(() => {
+    setSuccessMessage('Notas cargadas correctamente desde la API.')
+  }, 1000)
+}
