01b45471c8674a5deb818113ac35da2cf6a9563e
Charly <carlosgarita3110@gmail.com>
2024-09-23 13:29:11 -0600

    feat: add CSVUploadSection component for uploading CSV files
    This commit adds a new component called CSVUploadSection to the results module. The CSVUploadSection component is responsible for rendering a section where users can upload CSV files containing exam scores. It includes an InputField component for selecting the file and a handler function to process the uploaded file.

---
+++ management_client/src/components/modules/results/load_results/organisms/CSVUploadSection.jsx
@@ -0,0 +1,15 @@
+import InputField from '../../../../core/global/atoms/InputField'
+
+const CSVUploadSection = ({ handleCSVLoad }) => (
+  <div className='upload-section'>
+    <h2>Cargar Notas desde CSV</h2>
+    <InputField
+      field={{ type: 'file', name: 'csv', placeholder: 'Selecciona un archivo CSV' }}
+      handleChange={(e) => handleCSVLoad(e.target.files[0])}
+      autoComplete='off'
+      className='form-group'
+    />
+  </div>
+)
+
+export default CSVUploadSection
