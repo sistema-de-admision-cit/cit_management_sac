f63e28c24290592ecdcebfcc244dc27065edc6a9
Charly <carlosgarita3110@gmail.com>
2024-09-23 22:36:42 -0600

    refactor: update endpoint path in DaiExamQuestionsController and tests

--- cit-sac-back-end/src/main/java/cr/co/ctpcit/citsacbackend/rest/exams/dai/DaiExamQuestionsController.java
+++ cit-sac-back-end/src/main/java/cr/co/ctpcit/citsacbackend/rest/exams/dai/DaiExamQuestionsController.java
@@ -13,7 +13,7 @@ import java.util.List;
 
 
 @RestController
-@RequestMapping("/api/exams")
+@RequestMapping("/api/dai-exam-questions")
 @RequiredArgsConstructor
 public class DaiExamQuestionsController {
 
