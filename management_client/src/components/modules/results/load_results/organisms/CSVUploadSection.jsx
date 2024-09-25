a7a0d56ad3ce38b607c62915ad4d27ffcec70f91
Charly <carlosgarita3110@gmail.com>
2024-09-25 15:54:31 -0600

    feat: enhance CSV upload section with file selection and processing
    - Added state management for selected file using useState.
    - Implemented handleFileChange to update selected file.
    - Added button to process the uploaded file, triggering handleCSVLoad.

--- management_client/src/components/modules/results/load_results/organisms/CSVUploadSection.jsx
+++ management_client/src/components/modules/results/load_results/organisms/CSVUploadSection.jsx
@@ -1,16 +1,42 @@
+import Button from '../../../../core/global/atoms/Button'
 import InputField from '../../../../core/global/atoms/InputField'
 
-const CSVUploadSection = ({ handleCSVLoad }) => (
-  <div className='upload-section'>
-    <h2>Cargar Notas desde CSV</h2>
-    <InputField
-      field={{ type: 'file', name: 'csv', placeholder: 'Selecciona un archivo CSV' }}
-      handleChange={(e) => handleCSVLoad(e.target.files[0])}
-      autoComplete='off'
-      className='form-group'
-      accept='.csv, .xls, .xlsx'
-    />
-  </div>
-)
+import { useState } from 'react'
+
+const CSVUploadSection = ({ handleCSVLoad }) => {
+  const [selectedFile, setSelectedFile] = useState(null)
+
+  const handleFileChange = (e) => {
+    setSelectedFile(e.target.files[0])
+  }
+
+  const handleProcessFile = () => {
+    if (selectedFile) {
+      handleCSVLoad(selectedFile)
+    } else {
+      console.error('No file selected')
+    }
+  }
+
+  return (
+    <div className='upload-section'>
+      <h2>Cargar Notas desde CSV</h2>
+      <InputField
+        field={{ type: 'file', name: 'csv', placeholder: 'Selecciona un archivo CSV' }}
+        handleChange={handleFileChange}
+        autoComplete='off'
+        className='form-group'
+        accept='.csv, .xls, .xlsx'
+      />
+      <Button
+        onClick={handleProcessFile}
+        className='btn btn-primary'
+        disabled={!selectedFile}
+      >
+        Procesar
+      </Button>
+    </div>
+  )
+}
 
 export default CSVUploadSection
