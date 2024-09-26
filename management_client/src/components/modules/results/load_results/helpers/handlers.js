912a7854ab92b0769baef7c96d460ed8af7f5ef2
Charly <carlosgarita3110@gmail.com>
2024-09-26 11:30:47 -0600

    refactor: unify file upload handling logic
    - Refactored the file handling logic to streamline the process of handling CSV and XLSX files.
    - Introduced a generic `handleFile` function to avoid code duplication and improve maintainability.
    - The `handleXlsxFile` and `handleCsvFile` functions now call the shared `handleFile` function, passing the appropriate parser for each file type.
    - Improved error handling and file validation.

--- management_client/src/components/modules/results/load_results/helpers/handlers.js
+++ management_client/src/components/modules/results/load_results/helpers/handlers.js
@@ -10,74 +10,60 @@ export const fetchTrackTestScores = async (setSuccessMessage, setErrorMessage) =
 
 /* handle file upload */
 
-// handle XLSX files
-const handleXlsxFile = (file, setEnglishScores, setLoading, setErrorMessage) => {
-  // parse data from XLSX to json (xlsxToJson is a promise)
-  parseXlsxToArray(file)
-    .then((data) => {
-      // data is an array with the headers in the first position and the rows in the rest
-      const headers = data[0]
-      if (!validateFileStructure(headers)) {
-        setErrorMessage('Estructura de archivo inválida.')
-        setLoading(false)
-        return
-      }
+const handleFile = async (file, e, parser, setEnglishScores, setLoading, setErrorMessage) => {
+  try {
+    setLoading(true)
+    // Parse data using the provided parser (csv or xlsx)
+    const data = await parser(file)
 
-      const rows = data.slice(1)
-      const jsonData = convertToJson(headers, rows)
-      setEnglishScores(jsonData)
+    // Validate headers
+    const headers = data[0]
+    if (!validateFileStructure(headers)) {
+      setErrorMessage('Estructura de archivo inválida.')
+      e.target.value = null
       setLoading(false)
-    })
-    .catch((error) => {
-      console.error(error)
-      setErrorMessage('Error al leer el archivo XLSX.')
-      setLoading(false)
-    })
+      return
+    }
+
+    // Convert to JSON and set the scores
+    const rows = data.slice(1)
+    const jsonData = convertToJson(headers, rows)
+    setEnglishScores(jsonData)
+  } catch (error) {
+    console.error(error)
+    setErrorMessage('Error al leer el archivo.')
+  } finally {
+    setLoading(false)
+  }
 }
 
-// handle CSV files
-const handleCsvFile = (file, setEnglishScores, setLoading, setErrorMessage) => {
-  // parse data from CSV to json (csvToJson is a promise)
-  parseCsvToArray(file)
-    .then((data) => {
-      // data is an array with the headers in the first position and the rows in the rest
-      const headers = data[0]
-      if (!validateFileStructure(headers)) {
-        setErrorMessage('Estructura de archivo CSV inválida.')
-        setLoading(false)
-        return
-      }
+// Llamadas específicas
+const handleXlsxFile = (file, e, setEnglishScores, setLoading, setErrorMessage) => {
+  handleFile(file, e, parseXlsxToArray, setEnglishScores, setLoading, setErrorMessage)
+}
 
-      const rows = data.slice(1)
-      const jsonData = convertToJson(headers, rows)
-      setEnglishScores(jsonData)
-      setLoading(false)
-    })
-    .catch((error) => {
-      console.error(error)
-      setErrorMessage('Error al leer el archivo CSV.')
-      setLoading(false)
-    })
+const handleCsvFile = (file, e, setEnglishScores, setLoading, setErrorMessage) => {
+  handleFile(file, e, parseCsvToArray, setEnglishScores, setLoading, setErrorMessage)
 }
 
 // the file structure should have at least the following headers: 'ID', 'First and Middle Name', 'Last Name(s)', 'Last Test', 'Core'
 const validateFileStructure = (headers) => {
-  const requiredHeaders = ['ID', 'First and Middle Name', 'Last Name(s)', 'Last Test', 'Core']
+  const requiredHeaders = ['ID', 'First and Middle Name', 'Last Name(s)', 'Last test', 'Core']
   const missingHeaders = requiredHeaders.filter((header) => !headers.includes(header))
 
   return missingHeaders.length === 0
 }
 
 // this function is called when the user uploads a file
-export const handleEnglishScoresFileUpload = (file, setEnglishScores, setLoading, setErrorMessage) => {
+export const handleEnglishScoresFileUpload = (file, e, setEnglishScores, setLoading, setErrorMessage) => {
   setLoading(true)
 
   const fileExtension = file.name.split('.').pop().toLowerCase()
 
   if (fileExtension === 'xlsx' || fileExtension === 'xls') {
-    handleXlsxFile(file, setEnglishScores, setLoading, setErrorMessage)
+    handleXlsxFile(file, e, setEnglishScores, setLoading, setErrorMessage)
   } else if (fileExtension === 'csv') {
-    handleCsvFile(file, setEnglishScores, setLoading, setErrorMessage)
+    handleCsvFile(file, e, setEnglishScores, setLoading, setErrorMessage)
   } else {
     setErrorMessage('Formato de archivo no soportado.')
     setLoading(false)
