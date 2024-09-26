656c67a3c5095855b0e6670d6e6023fc69c07612
Charly <carlosgarita3110@gmail.com>
2024-09-26 11:58:00 -0600

    feat: enhance convertToJson to map headers to camelCase and filter empty rows
    - Added header mapping for camelCase conversion of keys.
    - Implemented filtering to remove rows with less than 2 columns.
    - Added date formatting for the 'lastTest' field to 'yyyy-MM-dd'.
    - Introduced helper function formatDateForJava to handle date formatting.

--- management_client/src/components/modules/results/load_results/helpers/helpers.js
+++ management_client/src/components/modules/results/load_results/helpers/helpers.js
@@ -35,13 +35,43 @@ export const parseCsvToArray = (file) => {
   })
 }
 
-// parse data from array to json
+// Mapping between headers and camelCase keys
+const headerMapping = {
+  ID: 'id',
+  'First and Middle Name': 'names',
+  'Last Name(s)': 'lastNames',
+  'Last test': 'lastTest',
+  Level: 'level',
+  Core: 'core'
+}
+
+// parse data from array to json and filter out unnecessary fields
 export const convertToJson = (headers, data) => {
+  // drop rows with less than 2 columns (empty rows)
+  data = data.filter((row) => row.length > 1)
+
   return data.map((row) => {
     const obj = {}
+
     headers.forEach((header, index) => {
-      obj[header] = row[index] || '' // Asignamos valor o un string vacío si falta algún dato
+      const camelCaseKey = headerMapping[header]
+
+      if (camelCaseKey) {
+        if (camelCaseKey === 'lastTest') {
+          // Formatear la fecha solo para 'lastTest'
+          obj[camelCaseKey] = formatDateForJava(row[index])
+        } else {
+          obj[camelCaseKey] = row[index]
+        }
+      }
     })
+
     return obj
   })
+}
+
+// Helper function to format dates to 'yyyy-MM-dd' (compatible with Instant in Java)
+const formatDateForJava = (dateString) => {
+  const [day, month, year] = dateString?.split('.')
+  return `${year}-${month}-${day}`
 }
