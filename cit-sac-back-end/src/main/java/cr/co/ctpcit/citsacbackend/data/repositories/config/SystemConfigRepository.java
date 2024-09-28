7fece3e95a9edad1c5418e32f24f6825bfea3bfa
Alessandro Cambronero Delgado <134018906+MSIALE2023@users.noreply.github.com>
2024-09-28 11:14:04 -0600

    refactor: Update notification code in SystemConfig repository

--- cit-sac-back-end/src/main/java/cr/co/ctpcit/citsacbackend/data/repositories/config/SystemConfigRepository.java
+++ cit-sac-back-end/src/main/java/cr/co/ctpcit/citsacbackend/data/repositories/config/SystemConfigRepository.java
@@ -4,6 +4,7 @@ import cr.co.ctpcit.citsacbackend.data.entities.config.SystemConfigEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -21,7 +22,19 @@ public interface SystemConfigRepository extends JpaRepository<SystemConfigEntity
 
   @Transactional
   @Modifying
-  @Query(
-          "UPDATE SystemConfigEntity s SET s.configValue = CASE s.configName " + "WHEN 'email_contact' THEN ?1 " + "WHEN 'email_notifications_contact' THEN ?2 " + "WHEN 'whatsapp_contact' THEN ?3 " + "WHEN 'office_contact' THEN ?4 " + "WHEN 'instagram_contact' THEN ?5 " + "WHEN 'facebook_contact' THEN ?6 " + "END")
-  void updateNotifications(String emailContact,String emailNotificationContact, String whatsappContact, String officeContact, String instagramContact, String facebookContact);
+  @Query("UPDATE SystemConfigEntity s SET s.configValue = CASE s.configName " +
+          "WHEN 'email_contact' THEN :emailContact " +
+          "WHEN 'email_notifications_contact' THEN :emailNotificationContact " +
+          "WHEN 'whatsapp_contact' THEN :whatsappContact " +
+          "WHEN 'office_contact' THEN :officeContact " +
+          "WHEN 'instagram_contact' THEN :instagramContact " +
+          "WHEN 'facebook_contact' THEN :facebookContact " +
+          "END WHERE s.configName IN ('email_contact', 'email_notifications_contact', 'whatsapp_contact', 'office_contact', 'instagram_contact', 'facebook_contact')")
+  void updateNotifications(@Param("emailContact") String emailContact,
+                           @Param("emailNotificationContact") String emailNotificationContact,
+                           @Param("whatsappContact") String whatsappContact,
+                           @Param("officeContact") String officeContact,
+                           @Param("instagramContact") String instagramContact,
+                           @Param("facebookContact") String facebookContact);
+
 }
