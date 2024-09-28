2121628c93d17caea03843d3026c18ca19330463
Alessandro Cambronero Delgado <134018906+MSIALE2023@users.noreply.github.com>
2024-09-28 11:17:39 -0600

    feat: Add test data for SQL notifications to SystemConfig table

---
+++ db/load_test_data_Notifications.sql
@@ -0,0 +1,10 @@
+use db_cit_test;
+
+INSERT INTO `db_cit_test`.`tbl_systemconfig` (`config_name`, `config_value`) VALUES ('email_contact', 'contactocit@ctpcit.com');
+INSERT INTO `db_cit_test`.`tbl_systemconfig` (`config_name`, `config_value`) VALUES ('email_notifications_contact', 'notificaciones@ctpcit.com');
+INSERT INTO `db_cit_test`.`tbl_systemconfig` (`config_name`, `config_value`) VALUES ('whatsapp_contact', '88090041');
+INSERT INTO `db_cit_test`.`tbl_systemconfig` (`config_name`, `config_value`) VALUES ('office_contact', '22370186');
+INSERT INTO `db_cit_test`.`tbl_systemconfig` (`config_name`, `config_value`) VALUES ('instagram_contact', 'ComplejoEducativoCIT');
+INSERT INTO `db_cit_test`.`tbl_systemconfig` (`config_name`, `config_value`) VALUES ('facebook_contact', 'ComplejoEducativoCIT');
+
+SELECT * FROM tbl_systemconfig;
