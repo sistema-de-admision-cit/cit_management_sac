97de9032ad52ff23c34b6e18dbfecf005ba26121
Charly <carlosgarita3110@gmail.com>
2024-09-30 14:35:24 -0600

    feat: update EnglishExamRepository procedure to include logging
    - Changed procedure from `usp_process_english_exam` to `usp_process_english_exam_and_log`

--- cit-sac-back-end/src/main/java/cr/co/ctpcit/citsacbackend/data/repositories/exam/english/EnglishExamRepository.java
+++ cit-sac-back-end/src/main/java/cr/co/ctpcit/citsacbackend/data/repositories/exam/english/EnglishExamRepository.java
@@ -13,10 +13,11 @@ import java.time.LocalDate;
 
 @Repository
 public interface EnglishExamRepository extends JpaRepository<EnglishExamEntity, Long> {
-
-  @Procedure(name = "usp_process_english_exam")
-  public void usp_process_english_exam(@Param("p_enrollment_id") Long enrollmentId,
+  @Modifying
+  @Transactional
+  @Procedure(name = "usp_process_english_exam_and_log")
+  public void usp_process_english_exam_and_log(@Param("p_first_name") String firstName,
+      @Param("p_last_names") String lastNames, @Param("p_exam_date") String examDate,
       @Param("p_tracktest_id") Long trackTestId, @Param("p_new_score") BigDecimal newScore,
-      @Param("p_exam_date") LocalDate examDate, @Param("p_level") String level,
-      @Param("p_process_id") Integer processId);
+      @Param("p_level") String level, @Param("p_process_id") Integer processId);
 }
