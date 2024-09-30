dddcd96390b39e7290ec514cc32f5c21a5404d8d
Charly <carlosgarita3110@gmail.com>
2024-09-30 11:26:43 -0600

    refactor: add @Repository annotation to EnrollmentRepository

--- cit-sac-back-end/src/main/java/cr/co/ctpcit/citsacbackend/data/repositories/inscriptions/EnrollmentRepository.java
+++ cit-sac-back-end/src/main/java/cr/co/ctpcit/citsacbackend/data/repositories/inscriptions/EnrollmentRepository.java
@@ -9,10 +9,12 @@ import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.jpa.repository.query.Procedure;
 import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
 import java.sql.Date;
 import java.util.Optional;
 
+@Repository
 public interface EnrollmentRepository extends JpaRepository<EnrollmentEntity, Long> {
   @Modifying
   @Transactional
