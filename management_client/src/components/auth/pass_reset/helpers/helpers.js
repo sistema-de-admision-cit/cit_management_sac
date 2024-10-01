8124a73e6364b51f5ca567c56f535f9edab341fa
Charly <carlosgarita3110@gmail.com>
2024-09-30 21:36:43 -0600

    feat: add password validation helper function
     The function validates the password based on the following criteria:
    - The password must have at least 8 characters, one letter, and one number.
    - The new password cannot be the same as the current password.
    - The new password must match the confirmed new password.

---
+++ management_client/src/components/auth/pass_reset/helpers/helpers.js
@@ -0,0 +1,16 @@
+export const validatePassword = (currentPassword, newPassword, confirmNewPassword) => {
+  const regex = /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/
+  if (!regex.test(newPassword)) {
+    return 'La contraseña debe tener al menos 8 caracteres, una letra y un número.'
+  }
+
+  if (currentPassword === newPassword) {
+    return 'La nueva contraseña no puede ser igual a la actual.'
+  }
+
+  if (newPassword !== confirmNewPassword) {
+    return 'Las contraseñas no coinciden.'
+  }
+
+  return true
+}
