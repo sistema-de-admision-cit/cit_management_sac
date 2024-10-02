9857f7947b4c16c2ad5eebcedf671a86e515b2d2
Charly <carlosgarita3110@gmail.com>
2024-10-02 17:05:43 -0600

    feat: implement authentication context with JWT and cookie management
    - Refactored `AuthProvider` to utilize `js-cookie` for managing authentication tokens and user data.
    - Updated user state to check for existing tokens in cookies to determine authentication status.
    - Implemented `login` function to authenticate users using Basic Auth, retrieve a JWT, and store it along with username and role in cookies.
    - Integrated JWT decoding to extract user information (username and role) for state management.
    - Added cookie expiration for 1 hour for stored credentials.
    - Updated `logout` function to clear user data and remove cookies upon logout.
    - Improved error handling during login attempts to provide user feedback for invalid credentials.

--- management_client/src/router/AuthProvider.jsx
+++ management_client/src/router/AuthProvider.jsx
@@ -1,19 +1,62 @@
 import { createContext, useState, useContext } from 'react'
 import PropTypes from 'prop-types'
+import Cookies from 'js-cookie' // Importa js-cookie
+import axios from 'axios'
+import { jwtDecode } from 'jwt-decode'
 
 // contexto con el estado de autenticación
 export const AuthContext = createContext({})
 
 export default function AuthProvider ({ children }) {
-  const [user, setUser] = useState({ isAuthenticated: true, name: 'John Doe', role: 'admin' }) // directamente autenticado para no tener que loguear (temporal)
+  const [user, setUser] = useState({
+    isAuthenticated: !!Cookies.get('token'), // Revisa si hay un token en las cookies
+    name: Cookies.get('username') || '', // Recupera el username desde la cookie, si existe
+    role: Cookies.get('role') || '', // Recupera el rol desde la cookie, si existe
+    token: Cookies.get('token') || null // Recupera el token desde la cookie, si existe
+  })
 
-  const login = () => {
-    // TODO: implementar lógica de login
-    setUser({ isAuthenticated: true, name: 'John Doe', role: 'admin' })
+  const login = (credentials, setErrorMessage, setIsDefaultPassword) => {
+    const loginUrl = `${import.meta.env.VITE_API_BASE_URL}${import.meta.env.VITE_LOGIN_ENDPOINT}` // URL de la API de login
+    // basic auth
+    const username = credentials.username
+    const password = credentials.password
+
+    const basicAuth = btoa(`${username}:${password}`)
+    const headers = {
+      Authorization: `Basic ${basicAuth}`
+    }
+
+    axios.post(loginUrl, {}, { headers })
+      .then(response => {
+        const token = response.data.token
+        const isDefaultPassword = response.data.isDefaultPassword
+        const decodedToken = jwtDecode(token)
+        setUser({
+          isAuthenticated: true,
+          name: decodedToken.sub, // Nombre de usuario
+          role: decodedToken.scope, // Rol de usuario
+          token
+        })
+
+        Cookies.set('token', token, { expires: 1 / 24 }) // Guarda el token en una cookie con duración de 1 hora
+        Cookies.set('username', decodedToken.sub, { expires: 1 / 24 }) // Guarda el username
+        Cookies.set('role', decodedToken.scope, { expires: 1 / 24 }) // Guarda el rol
+
+        if (isDefaultPassword) {
+          setIsDefaultPassword(true)
+        }
+      })
+      .catch(error => {
+        console.error('Error en la autenticación:', error)
+        setErrorMessage('Credenciales inválidas')
+      })
   }
 
   const logout = () => {
-    setUser({ isAuthenticated: false, name: '', role: '' })
+    setUser({ isAuthenticated: false, name: '', role: '', token: null })
+    Cookies.remove('token')
+    Cookies.remove('username')
+    Cookies.remove('role')
   }
 
   return (
