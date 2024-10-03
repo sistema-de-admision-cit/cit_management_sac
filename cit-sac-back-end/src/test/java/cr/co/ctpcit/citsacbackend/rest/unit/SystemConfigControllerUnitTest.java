85c7185c484bc320105cecef458580eb36ebb103
Alessandro Cambronero Delgado <134018906+MSIALE2023@users.noreply.github.com>
2024-10-02 20:35:22 -0600

    test: Completed integration and unit tests for SystemConfigController.

--- cit-sac-back-end/src/test/java/cr/co/ctpcit/citsacbackend/rest/unit/SystemConfigControllerUnitTest.java
+++ cit-sac-back-end/src/test/java/cr/co/ctpcit/citsacbackend/rest/unit/SystemConfigControllerUnitTest.java
@@ -1,55 +1,142 @@
 package cr.co.ctpcit.citsacbackend.rest.unit;
 
+import static org.mockito.ArgumentMatchers.*;
+import static org.mockito.Mockito.*;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.httpBasic;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
 import cr.co.ctpcit.citsacbackend.data.entities.config.SystemConfigEntity;
-import cr.co.ctpcit.citsacbackend.data.repositories.config.SystemConfigRepository;
+import cr.co.ctpcit.citsacbackend.logic.dto.auth.AuthResponseDto;
 import cr.co.ctpcit.citsacbackend.logic.dto.config.SystemConfigDto;
-import cr.co.ctpcit.citsacbackend.logic.mappers.config.SystemConfigMapper;
 import cr.co.ctpcit.citsacbackend.logic.services.SystemConfigServiceImplementation;
-import cr.co.ctpcit.citsacbackend.logic.services.storage.FileSystemStorageService;
+import cr.co.ctpcit.citsacbackend.rest.config.SystemConfigController;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.transaction.annotation.Transactional;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.*;
+import java.util.Arrays;
+import java.util.List;
 
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureMockMvc
+@Transactional
+@Rollback
 public class SystemConfigControllerUnitTest {
 
-  @Mock
-  private SystemConfigRepository systemConfigRepository;
+  @Autowired
+  private MockMvc mockMvc;
 
-  @InjectMocks
-  private SystemConfigServiceImplementation systemConfigService;
+  @Autowired
+  private ObjectMapper objectMapper;
 
   @MockBean
-  private FileSystemStorageService serviceStorage;
+  private SystemConfigServiceImplementation systemConfigService;
+
+  private List<SystemConfigEntity> examPercentages;
+  private List<SystemConfigEntity> notifications;
+
+  AuthResponseDto authResponseDto;
 
   @BeforeEach
-  void setUp() {
+  void setUp() throws Exception {
     MockitoAnnotations.openMocks(this);
+
+
+    // Mock notifications list
+    notifications = Arrays.asList(
+            new SystemConfigEntity(1, "email_contact", "complejoEducativo@ctpcit.com"),
+            new SystemConfigEntity(2, "email_notifications_contact", "notificacionesCIT@ctpcit.com"),
+            new SystemConfigEntity(3, "whatsapp_contact", "88950252"),
+            new SystemConfigEntity(4, "office_contact", "22370186"),
+            new SystemConfigEntity(5, "instagram_contact", "ComplejoEducativoCIT"),
+            new SystemConfigEntity(6, "facebook_contact", "ComplejoEducativoCIT")
+    );
+
+    MvcResult result = this.mockMvc.perform(
+                    post("/api/auth/login").with(httpBasic("sysadmin@cit.co.cr", "campus12")))
+            .andExpect(status().isOk()).andReturn();
+
+    authResponseDto =
+            objectMapper.readValue(result.getResponse().getContentAsString(), AuthResponseDto.class);
+  }
+
+
+  @Test
+  void getNotifications() throws Exception {
+    when(systemConfigService.getNotifications("contact")).thenReturn(notifications);
+
+    mockMvc.perform(get("/api/system-config/get-notifications").header("Authorization",
+                    "Bearer " + authResponseDto.token()).contentType(MediaType.APPLICATION_JSON))
+            .andExpect(status().isOk())
+            .andExpect(jsonPath("$[0].configName").value("email_contact"))
+            .andExpect(jsonPath("$[1].configName").value("email_notifications_contact"))
+            .andExpect(jsonPath("$[2].configName").value("whatsapp_contact"))
+            .andExpect(jsonPath("$[3].configName").value("office_contact"))
+            .andExpect(jsonPath("$[4].configName").value("instagram_contact"))
+            .andExpect(jsonPath("$[5].configName").value("facebook_contact"));
   }
 
   @Test
-  void addSystemConfigTest() {
-    SystemConfigDto systemConfigDto = new SystemConfigDto(null, "examenDai", "40");
-    SystemConfigEntity systemConfigEntity = SystemConfigMapper.toEntity(systemConfigDto);
-    systemConfigEntity.setId(1);
+  void updateNotifications() throws Exception {
+    List<SystemConfigEntity> updatedNotifications = Arrays.asList(
+            new SystemConfigEntity(1, "email_contact", "complejoEducativo@ctpcit.com"),
+            new SystemConfigEntity(2, "email_notifications_contact", "notificacionesCIT@ctpcit.com"),
+            new SystemConfigEntity(3, "whatsapp_contact", "88950252"),
+            new SystemConfigEntity(4, "office_contact", "22370186"),
+            new SystemConfigEntity(5, "instagram_contact", "ComplejoEducativoCIT"),
+            new SystemConfigEntity(6, "facebook_contact", "ComplejoEducativoCIT")
+    );
 
-    when(systemConfigRepository.save(any(SystemConfigEntity.class))).thenReturn(systemConfigEntity);
+    doNothing().when(systemConfigService).updateNotifications(
+            "complejoEducativo@ctpcit.com",
+            "notificacionesCIT@ctpcit.com",
+            "88950252",
+            "22370186",
+            "ComplejoEducativoCIT",
+            "ComplejoEducativoCIT"
+    );
+    when(systemConfigService.getNotifications("contact")).thenReturn(updatedNotifications);
 
-
-    SystemConfigDto result = systemConfigService.addSystemConfig(systemConfigDto);
-
-    assertNotNull(result);
-    assertEquals("examenDai", result.configName());
-    assertEquals("40", result.configValue());
-    assertEquals(1, result.id());
-
-    verify(systemConfigRepository, times(1)).save(any(SystemConfigEntity.class));
+    mockMvc.perform(put("/api/system-config/update-notifications")
+                    .param("email_contact", "complejoEducativo@ctpcit.com")
+                    .param("email_notifications_contact", "notificacionesCIT@ctpcit.com")
+                    .param("whatsapp_contact", "88950252")
+                    .param("office_contact", "22370186")
+                    .param("instagram_contact", "ComplejoEducativoCIT")
+                    .param("facebook_contact", "ComplejoEducativoCIT")
+                    .header("Authorization", "Bearer " + authResponseDto.token())
+                    .contentType(MediaType.APPLICATION_JSON))
+            .andExpect(status().isOk())
+            .andExpect(jsonPath("$[0].configName").value("email_contact"))
+            .andExpect(jsonPath("$[0].configValue").value("complejoEducativo@ctpcit.com"))
+            .andExpect(jsonPath("$[1].configName").value("email_notifications_contact"))
+            .andExpect(jsonPath("$[1].configValue").value("notificacionesCIT@ctpcit.com"))
+            .andExpect(jsonPath("$[2].configName").value("whatsapp_contact"))
+            .andExpect(jsonPath("$[2].configValue").value("88950252"))
+            .andExpect(jsonPath("$[3].configName").value("office_contact"))
+            .andExpect(jsonPath("$[3].configValue").value("22370186"))
+            .andExpect(jsonPath("$[4].configName").value("instagram_contact"))
+            .andExpect(jsonPath("$[4].configValue").value("ComplejoEducativoCIT"))
+            .andExpect(jsonPath("$[5].configName").value("facebook_contact"))
+            .andExpect(jsonPath("$[5].configValue").value("ComplejoEducativoCIT"));
   }
+
 }
+
+
+
