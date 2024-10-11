74a78c82c67e5cd55c14e0c62ae51ac01346c7d9
Charly <carlosgarita3110@gmail.com>
2024-10-10 21:59:20 -0600

    refactor: remove `fetchTrackTestScores` function  in handlers.js

--- management_client/src/components/modules/results/load_results/helpers/handlers.js
+++ management_client/src/components/modules/results/load_results/helpers/handlers.js
@@ -1,14 +1,6 @@
 import axios from 'axios'
 import { convertToJson, formatLogMessage, parseCsvToArray, parseXlsxToArray } from './helpers'
 
-export const fetchTrackTestScores = async (setSuccessMessage, setErrorMessage) => {
-  // todo: fetch track test scores from the API
-  // axios.get('/api/track-test-scores')
-  setTimeout(() => {
-    setSuccessMessage('Notas cargadas correctamente desde la API.')
-  }, 1000)
-}
-
 /* handle file upload */
 
 const handleFile = async (file, e, parser, setEnglishScores, setLoading, setErrorMessage) => {
