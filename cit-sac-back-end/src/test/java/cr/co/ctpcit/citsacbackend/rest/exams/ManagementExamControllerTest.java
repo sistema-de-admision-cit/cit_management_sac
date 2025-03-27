641ad13ce226690dc5d48b50af17db3077202f95
Alessandro Cambronero Delgado <134018906+MSIALE2023@users.noreply.github.com>
2025-03-27 09:30:23 -0600

    test: Added test cases for non-existent student and student with no exams in ManagementExamControllerTest

--- cit-sac-back-end/src/test/java/cr/co/ctpcit/citsacbackend/rest/exams/ManagementExamControllerTest.java
+++ cit-sac-back-end/src/test/java/cr/co/ctpcit/citsacbackend/rest/exams/ManagementExamControllerTest.java
@@ -5,6 +5,7 @@ import com.jayway.jsonpath.JsonPath;
 import cr.co.ctpcit.citsacbackend.data.enums.ExamType;
 import net.minidev.json.JSONArray;
 import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -23,6 +24,7 @@ class ManagementExamControllerTest {
   private TestRestTemplate restTemplate;
 
   @Test
+  @Order(1)
   void getAcademicExamsOfAStudent() {
     ResponseEntity<String> response =
         restTemplate.getForEntity("/api/management-exams/academic-exams/270456789", String.class);
@@ -33,6 +35,29 @@ class ManagementExamControllerTest {
 
     int enrollmentCount = documentContext.read("$.length()");
     assertThat(enrollmentCount).isEqualTo(1);
+  }
+
+  @Test
+  @Order(2)
+  void getAcademicExamsOfANonExistentStudent() {
+    ResponseEntity<String> response =
+            restTemplate.getForEntity("/api/management-exams/academic-exams/999999999", String.class);
+
+    assertThat(response.getStatusCode()).isEqualTo(HttpStatus.NOT_FOUND);
+  }
+
+  @Test
+  @Order(3)
+  void getAcademicExamsOfAStudentWithNoExams() {
+    ResponseEntity<String> response =
+            restTemplate.getForEntity("/api/management-exams/academic-exams/230987654", String.class);
+
+    assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+
+    DocumentContext documentContext = JsonPath.parse(response.getBody());
+
+    int enrollmentCount = documentContext.read("$.length()");
+    assertThat(enrollmentCount).isEqualTo(0);
   }
 
 }
