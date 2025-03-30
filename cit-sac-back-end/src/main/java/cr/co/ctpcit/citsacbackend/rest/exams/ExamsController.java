3f8288d203336de44bf1f1d9f201ca4f1603847d
Charly <carlosgarita3110@gmail.com>
2025-03-30 16:36:02 -0600

    feat: add endpoints for retrieving and updating English exam scores

--- cit-sac-back-end/src/main/java/cr/co/ctpcit/citsacbackend/rest/exams/ExamsController.java
+++ cit-sac-back-end/src/main/java/cr/co/ctpcit/citsacbackend/rest/exams/ExamsController.java
@@ -4,11 +4,14 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import cr.co.ctpcit.citsacbackend.logic.dto.exams.academic.ExamAcaDto;
 import cr.co.ctpcit.citsacbackend.logic.dto.exams.dai.ExamDaiDto;
 import cr.co.ctpcit.citsacbackend.logic.services.exams.ExamsService;
+import cr.co.ctpcit.citsacbackend.logic.services.exams.english.EnglishExamService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/exams")
@@ -16,6 +19,7 @@ import org.springframework.web.bind.annotation.*;
 @Validated
 public class ExamsController {
   private final ExamsService examsService;
+  private final EnglishExamService englishExamService;
 
   @GetMapping("/academic-exam/{id}")
   public ResponseEntity<ExamAcaDto> getAcademicExam(@PathVariable String id) {
@@ -47,4 +51,20 @@ public class ExamsController {
     return ResponseEntity.noContent().build();
   }
 
+  @GetMapping("english-exam/get-exams")
+  public ResponseEntity<List<EnglishExamEntity>> getEnglishExamScores() {
+    return ResponseEntity.ok(englishExamService.getAll());
+  }
+
+  @PostMapping("english-exam/update-scores")
+  public ResponseEntity<List<EnglishExamLogDto>> uploadEnglishScores(
+      @RequestBody List<EnglishScoreEntryDTO> englishScores) {
+    List<EnglishExamLogDto> logs = englishExamService.processEnglishScores(englishScores);
+
+    if (logs.isEmpty()) {
+      return ResponseEntity.badRequest().body(logs);
+    }
+
+    return ResponseEntity.ok(logs);
+  }
 }
