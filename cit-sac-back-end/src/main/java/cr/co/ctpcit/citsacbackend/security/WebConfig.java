57c20d4e6ece19ff9ee26926acd851774a00d612
Alessandro Cambronero Delgado <134018906+MSIALE2023@users.noreply.github.com>
2025-04-14 13:29:49 -0600

    fix: change the WebConfig

--- cit-sac-back-end/src/main/java/cr/co/ctpcit/citsacbackend/security/WebConfig.java
+++ cit-sac-back-end/src/main/java/cr/co/ctpcit/citsacbackend/security/WebConfig.java
@@ -8,7 +8,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class WebConfig implements WebMvcConfigurer {
   @Override
   public void addCorsMappings(CorsRegistry registry) {
-    registry.addMapping("/**").allowedOriginPatterns("*")
-        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS").allowedOrigins("*");
+    registry.addMapping("/**").allowedOriginPatterns("http://localhost:5173")
+        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS");
   }
 }
